9a503632459ef6a903a1de11c8553377822a2059
James Boe <jboe@easy-insight.com>
2009-02-23 20:09:57 +0000

    latest changes

---
+++ java/src/com/easyinsight/analysis/ListRow.java
@@ -0,0 +1,22 @@
+package com.easyinsight.analysis;
+
+import com.easyinsight.core.Value;
+
+import java.io.Serializable;
+
+/**
+ * User: James Boe
+ * Date: Jan 9, 2008
+ * Time: 9:04:57 PM
+ */
+public class ListRow implements Serializable {
+    private Value[] values;
+
+    public Value[] getValues() {
+        return values;
+    }
+
+    public void setValues(Value[] values) {
+        this.values = values;
+    }
+}
