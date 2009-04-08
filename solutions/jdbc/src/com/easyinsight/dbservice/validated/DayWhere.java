77586bc2e8d6cac5e281450a0955841c986c3487
James Boe <jboe@easy-insight.com>
2009-04-08 16:35:17 +0000

    added DayWhere to jdbc solution

---
+++ solutions/jdbc/src/com/easyinsight/dbservice/validated/DayWhere.java
@@ -0,0 +1,184 @@
+/**
+ * DayWhere.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.easyinsight.dbservice.validated;
+
+public class DayWhere  implements java.io.Serializable {
+    private int dayOfYear;
+
+    private java.lang.String key;
+
+    private int year;
+
+    public DayWhere() {
+    }
+
+    public DayWhere(
+           int dayOfYear,
+           java.lang.String key,
+           int year) {
+           this.dayOfYear = dayOfYear;
+           this.key = key;
+           this.year = year;
+    }
+
+
+    /**
+     * Gets the dayOfYear value for this DayWhere.
+     * 
+     * @return dayOfYear
+     */
+    public int getDayOfYear() {
+        return dayOfYear;
+    }
+
+
+    /**
+     * Sets the dayOfYear value for this DayWhere.
+     * 
+     * @param dayOfYear
+     */
+    public void setDayOfYear(int dayOfYear) {
+        this.dayOfYear = dayOfYear;
+    }
+
+
+    /**
+     * Gets the key value for this DayWhere.
+     * 
+     * @return key
+     */
+    public java.lang.String getKey() {
+        return key;
+    }
+
+
+    /**
+     * Sets the key value for this DayWhere.
+     * 
+     * @param key
+     */
+    public void setKey(java.lang.String key) {
+        this.key = key;
+    }
+
+
+    /**
+     * Gets the year value for this DayWhere.
+     * 
+     * @return year
+     */
+    public int getYear() {
+        return year;
+    }
+
+
+    /**
+     * Sets the year value for this DayWhere.
+     * 
+     * @param year
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof DayWhere)) return false;
+        DayWhere other = (DayWhere) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            this.dayOfYear == other.getDayOfYear() &&
+            ((this.key==null && other.getKey()==null) || 
+             (this.key!=null &&
+              this.key.equals(other.getKey()))) &&
+            this.year == other.getYear();
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        _hashCode += getDayOfYear();
+        if (getKey() != null) {
+            _hashCode += getKey().hashCode();
+        }
+        _hashCode += getYear();
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(DayWhere.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://basicauth.api.easyinsight.com/", "dayWhere"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("dayOfYear");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "dayOfYear"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("key");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "key"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("year");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "year"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
