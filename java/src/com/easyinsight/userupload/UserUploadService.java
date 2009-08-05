653a5e6f7a65b31d6dfe08bef670ff15bdd23b5b
James Boe <jboe@easy-insight.com>
2009-08-05 20:35:12 +0000

    fixed issue with removing subscription to data sources

--- java/src/com/easyinsight/userupload/UserUploadService.java
+++ java/src/com/easyinsight/userupload/UserUploadService.java
@@ -346,7 +346,7 @@ public class UserUploadService implements IUserUploadService {
             if (role == Roles.OWNER) {
                 FeedDefinition feedDefinition = feedStorage.getFeedDefinitionData(dataFeedID, conn);
                 feedDefinition.delete(conn);
-            } else if (role == Roles.SUBSCRIBER) {
+            } else if (role == Roles.SUBSCRIBER || role == Roles.SHARER) {
                 deleteUserFeedLink(SecurityUtil.getUserID(), dataFeedID, conn);
             } else {
                 throw new SecurityException();
