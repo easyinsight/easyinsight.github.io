1476c6a67060051ea2beb6383d53a3aa8a6da27c
James Boe <jboe@easy-insight.com>
2009-08-25 17:07:02 +0000

    changes to try and capture user ID on the session

---
+++ flex/src/com/easyinsight/framework/TokenRedirector.as
@@ -0,0 +1,41 @@
+package com.easyinsight.framework {
+import mx.controls.Alert;
+import mx.rpc.events.FaultEvent;
+import mx.rpc.events.ResultEvent;
+import mx.rpc.remoting.RemoteObject;
+
+public class TokenRedirector {
+
+    private var tokenService:RemoteObject;
+
+    private var _type:int;
+
+    public function TokenRedirector() {
+        tokenService = new RemoteObject();
+        tokenService.destination = "tokenService";
+        tokenService.setToken.addEventListener(ResultEvent.RESULT, onTokenResult);
+        tokenService.setToken.addEventListener(FaultEvent.FAULT, onFault);
+    }
+
+    public function set type(value:int):void {
+        _type = value;
+    }
+
+    private function onFault(event:FaultEvent):void {
+        Alert.show(event.fault.faultString);
+    }
+
+    private function onTokenResult(event:ResultEvent):void {
+        var result:String = tokenService.setToken.lastResult as String;
+        if (result == null) {
+            Alert.show("redirect at this point");
+        } else {
+            Alert.show(result);
+        }
+    }
+
+    public function onURL(url:String):void {
+        tokenService.setToken.send(_type, url);
+    }
+}
+}
--- java/src/com/easyinsight/users/TokenService.java
+++ java/src/com/easyinsight/users/TokenService.java
@@ -7,9 +7,14 @@ import com.google.gdata.client.spreadsheet.SpreadsheetService;
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
 import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 import com.google.gdata.util.ServiceException;
+import com.google.gdata.util.AuthenticationException;
 
+import javax.servlet.http.HttpServletResponse;
 import java.net.URL;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Map;
+import java.util.Iterator;
 
 /**
  * User: jamesboe
@@ -17,15 +22,83 @@ import java.io.IOException;
  * Time: 2:25:19 PM
  */
 public class TokenService {
+
+    public String setToken(int type, String url) {
+        String queryString = url.substring(url.indexOf("?"));
+        String token = AuthSubUtil.getTokenFromReply(queryString);
+        if (token == null) {
+          return "No token specified.";
+
+        }
+
+        // Exchange the token for a session token
+        String sessionToken;
+        try {
+          sessionToken =
+            AuthSubUtil.exchangeForSessionToken(token,
+                                                Utility.getPrivateKey());
+        } catch (IOException e1) {
+          return "Exception retrieving session token.";
+        } catch (GeneralSecurityException e1) {
+          return "Security error while retrieving session token.";
+        } catch (AuthenticationException e) {
+          return "Server rejected one time use token.";
+        }
+
+        try {
+          // Sanity checking usability of token
+          Map<String, String> info =
+            AuthSubUtil.getTokenInfo(sessionToken, Utility.getPrivateKey());
+          for (Iterator<String> iter = info.keySet().iterator(); iter.hasNext();) {
+            String key = iter.next();
+            System.out.println("\t(key, value): (" + key + ", " + info.get(key)
+                               + ")");
+          }
+        } catch (IOException e1) {
+          return "Exception retrieving info for session token.";
+        } catch (GeneralSecurityException e1) {
+          return "Security error while retrieving session token info.";
+        } catch (AuthenticationException e) {
+          return "Auth error retrieving info for session token: " +
+                         e.getMessage();
+        }
+
+        // Retrieve the authentication cookie to identify user
+        /*String principal = Utility.getCookieValueWithName(req.getCookies(), Utility.LOGIN_COOKIE_NAME);
+        if (principal == null) {
+          resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                         "Unidentified principal.");
+          return;
+        }*/
+        //SecurityUtil.getUserID(false);
+
+        Token tokenObject = new Token();
+        tokenObject.setUserID(SecurityUtil.getUserID());
+        tokenObject.setTokenType(type);
+        tokenObject.setTokenValue(sessionToken);
+        new TokenStorage().saveToken(tokenObject);
+        return null;
+    }
+
     public boolean isTokenEstablished(int type) {
-        Token token = new TokenStorage().getToken(SecurityUtil.getUserID(), type);
-        return token != null;
+        try {
+            Token token = new TokenStorage().getToken(SecurityUtil.getUserID(), type);
+            return token != null;
+        } catch (Exception e) {
+            LogClass.error(e);
+            throw new RuntimeException(e);
+        }
     }
 
     public String getAuthSubURL() {
-        String nextURL = "https://staging.easy-insight.com/app/TokenRedirect";
-        String scope = "http://spreadsheets.google.com/feeds/";
-        return AuthSubUtil.getRequestUrl(nextURL, scope, false, true);
+        try {
+            String nextURL = "https://staging.easy-insight.com/app/#redirectID=1";
+            String scope = "http://spreadsheets.google.com/feeds/";
+            return AuthSubUtil.getRequestUrl(nextURL, scope, false, true);
+        } catch (Exception e) {
+            LogClass.error(e);
+            throw new RuntimeException(e);
+        }
     }
 
     public String test() {
@@ -34,7 +107,7 @@ public class TokenService {
             Token token = new TokenStorage().getToken(SecurityUtil.getUserID(), TokenStorage.GOOGLE_DOCS_TOKEN);
             if (token != null) {
                 spreadsheetService.setAuthSubToken(token.getTokenValue());
-                SpreadsheetFeed spreadsheetFeed = null;
+                SpreadsheetFeed spreadsheetFeed;
                 try {
                     URL feedUrl = new URL("http://spreadsheets.google.com/feeds/spreadsheets/private/full");
                     spreadsheetFeed = spreadsheetService.getFeed(feedUrl, SpreadsheetFeed.class);
