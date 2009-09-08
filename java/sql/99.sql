e9abdb82b014940ce81bf8c1038e6ee25a8b81ac
James Boe <jboe@easy-insight.com>
2009-09-08 02:15:47 +0000

    fixed missing cascade

--- java/sql/99.sql
+++ java/sql/99.sql
@@ -4,5 +4,5 @@ create table highrise (
   feed_id bigint(20) not null,
   url varchar(255) not null,
   primary key(highrise_id),
-  constraint highrise_ibfk1 foreign key (feed_id) references data_feed (data_feed_id)
+  constraint highrise_ibfk1 foreign key (feed_id) references data_feed (data_feed_id) on delete cascade
 );
