3cdf8e8374898d81effa0a4aac2d8173d4087f73
James Boe <jboe@easy-insight.com>
2010-03-09 22:53:20 +0000

    argh

---
+++ java/src/com/easyinsight/datafeeds/netresults/NetResultsDataSource.java
@@ -0,0 +1,26 @@
+package com.easyinsight.datafeeds.netresults;
+
+import com.easyinsight.client.netresults.*;
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Stub;
+
+import java.math.BigInteger;
+
+/**
+ * User: jamesboe
+ * Date: Mar 9, 2010
+ * Time: 3:16:02 PM
+ */
+public class NetResultsDataSource {
+    public static void main(String[] args) throws Exception {
+        NRAPILocator locator = new NRAPILocator();
+        locator.setNRAPIPortEndpointAddress("https://apps.net-results.com/soap/v1/NRAPI.wsdl");
+
+        NRAPIPortType port = locator.getNRAPIPort();
+        ((Stub) port)._setProperty(Call.USERNAME_PROPERTY, "jboe@easy-insight.com");
+        ((Stub) port)._setProperty(Call.PASSWORD_PROPERTY, "Ichai1g");
+
+        Contact contact = port.getContactByContactId(BigInteger.valueOf(8418636));
+        System.out.println("blah");
+    }
+}
