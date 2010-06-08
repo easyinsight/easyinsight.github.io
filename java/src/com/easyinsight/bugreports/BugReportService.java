0f0a87e0fad57fc7797824f48f1caac58aa44502
James Boe <jboe@easy-insight.com>
2010-06-08 17:11:34 +0000

    better flex error handling

--- java/src/com/easyinsight/bugreports/BugReportService.java
+++ java/src/com/easyinsight/bugreports/BugReportService.java
@@ -1,7 +1,12 @@
 package com.easyinsight.bugreports;
 
+import com.easyinsight.config.ConfigLoader;
+import com.easyinsight.email.AuthSMTPConnection;
 import com.easyinsight.security.SecurityUtil;
 import com.easyinsight.datafeeds.basecamp.BaseCampBugReportProvider;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,5 +20,23 @@ public class BugReportService {
         if(username == null) return;
         BaseCampBugReportProvider  bugReport = new BaseCampBugReportProvider();
         bugReport.reportBug(bugArea, bugText, username);
+    }
+
+    public void reportFlexError(String errorMessage, String stackTrace) {
+        if(ConfigLoader.instance().isProduction()) {
+            String username = null;
+            try {
+                username = SecurityUtil.getUserName();
+            } catch(Exception e1) {
+            }
+            try {
+                new AuthSMTPConnection().sendSSLMessage("errors@easy-insight.com",
+                        "Error! " + (username != null ? username : "Unknown") + ": " + errorMessage, stackTrace, "donotreply@easy-insight.com");
+            }
+            catch(Exception ex) {
+                // do nothing, wtf do you do at this point?
+                ex.printStackTrace();
+            }
+        }
     }
 }
