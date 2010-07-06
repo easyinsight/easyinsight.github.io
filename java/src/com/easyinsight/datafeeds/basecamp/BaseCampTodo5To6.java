a79c352ac4248251a05193bdcd4533ac7488a9a3
James Boe <jboe@easy-insight.com>
2010-07-06 01:55:28 +0000

    arghs

--- java/src/com/easyinsight/datafeeds/basecamp/BaseCampTodo5To6.java
+++ java/src/com/easyinsight/datafeeds/basecamp/BaseCampTodo5To6.java
@@ -22,7 +22,7 @@ public class BaseCampTodo5To6 extends DataSourceMigration {
 
     @Override
     public void migrate(Map<String, Key> keys, EIConnection conn) throws SQLException {
-        addAnalysisItem(new AnalysisDateDimension(new NamedKey(BaseCampTodoSource.PROJECT_CREATION_DATE), true, AnalysisDateDimension.DAY_LEVEL));
+        //addAnalysisItem(new AnalysisDateDimension(new NamedKey(BaseCampTodoSource.PROJECT_CREATION_DATE), true, AnalysisDateDimension.DAY_LEVEL));
     }
 
     @Override
