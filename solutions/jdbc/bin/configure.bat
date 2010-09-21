ed8a8ef76da6d2af9e29cba5e63922ee373a18b6
James Boe <jboe@easy-insight.com>
2010-09-21 19:36:09 +0000

    argh

--- solutions/jdbc/bin/configure.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set CLASSPATH=
-for %%i in (..\lib\*.jar) do call cpappend.bat %%i
-set CLASSPATH=%JARS%
-
-"%JAVA_HOME%"\bin\java -classpath "%CLASSPATH%" com.easyinsight.dbservice.Configuration
