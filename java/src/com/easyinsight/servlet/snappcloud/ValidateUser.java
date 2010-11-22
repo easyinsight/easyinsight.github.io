4f98141103430bba9ea53ab5ecc3df624e402798
Alan Baldwin <abaldwin@easy-insight.com>
2010-11-22 18:47:23 +0000

    servlet update

---
+++ java/src/com/easyinsight/servlet/snappcloud/ValidateUser.java
@@ -0,0 +1,29 @@
+package com.easyinsight.servlet.snappcloud;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abaldwin
+ * Date: Nov 22, 2010
+ * Time: 11:06:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ValidateUser extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        throw new ServletException();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("username");
+        System.out.println(name);
+        response.setStatus(200);
+    }
+}
