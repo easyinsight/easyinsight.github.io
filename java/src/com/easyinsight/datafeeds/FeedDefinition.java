e6a18cffd7fadfe85028981d0ce6ac0b9b6e5b55
James Boe <jboe@easy-insight.com>
2010-11-29 23:10:42 +0000

    fixed data source delete

--- java/src/com/easyinsight/datafeeds/FeedDefinition.java
+++ java/src/com/easyinsight/datafeeds/FeedDefinition.java
@@ -559,8 +559,6 @@ public class FeedDefinition implements Cloneable, Serializable {
         onDelete(conn);
         
         // cascade into base classes - see http://bugs.mysql.com/bug.php?id=13102 as to why this code sucks.
-        PreparedStatement deleteTodosStmt = conn.prepareStatement("delete from todo_base where todo_id in (select todo_id from configure_data_feed_todo where data_source_id = ?)");
-        PreparedStatement deleteAsyncTasksStmt = conn.prepareStatement("DELETE FROM scheduled_task WHERE scheduled_task_id in (select scheduled_task_id from server_refresh_task where data_source_id = ?)");
         Session session = Database.instance().createSession(conn);
 
         /*List reportObjects = session.createQuery("from AnalysisDefinition where dataFeedID = ?").setLong(0, getDataFeedID()).list();*/
@@ -586,12 +584,10 @@ public class FeedDefinition implements Cloneable, Serializable {
         session.flush();
         session.close();
         PreparedStatement deleteStmt = conn.prepareStatement("DELETE FROM DATA_FEED WHERE DATA_FEED_ID = ?");
-        deleteTodosStmt.setLong(1, dataFeedID);
-        deleteAsyncTasksStmt.setLong(1, dataFeedID);
+
         deleteStmt.setLong(1, dataFeedID);
 
-        deleteTodosStmt.executeUpdate();
-        deleteAsyncTasksStmt.executeUpdate();
+
         deleteStmt.executeUpdate();
     }
 
