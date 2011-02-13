fde9ec6ca43556ab427702917d2cf0a5e7e9183b
James Boe <jboe@easy-insight.com>
2011-02-13 02:54:36 +0000

    caching fix

--- java/src/com/easyinsight/export/HtmlResultCache.java
+++ java/src/com/easyinsight/export/HtmlResultCache.java
@@ -1,5 +1,8 @@
 package com.easyinsight.export;
 
+import com.easyinsight.logging.LogClass;
+import org.apache.jcs.JCS;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,9 +15,6 @@ public class HtmlResultCache {
 
     private static HtmlResultCache instance;
 
-    private Map<Long, Object> locks = new HashMap<Long, Object>();
-    private Map<Long, byte[]> results = new HashMap<Long, byte[]>();
-
     public static HtmlResultCache getInstance() {
         return instance;
     }
@@ -23,24 +23,37 @@ public class HtmlResultCache {
         instance = new HtmlResultCache();
     }
 
-    public byte[] waitForResults(long id) throws InterruptedException {
-        final Object lock = new Object();
-        locks.put(id, lock);
-        synchronized(lock) {
-            System.out.println("waiting...");
-            lock.wait(60000);
-            System.out.println("got notified!");
-            return results.remove(id);
+    private JCS resultCache = getCache("htmlcache");
+
+    private JCS getCache(String cacheName) {
+
+        try {
+            return JCS.getInstance(cacheName);
+        } catch (Exception e) {
+            LogClass.error(e);
         }
+        return null;
+    }
+
+    public byte[] waitForResults(long id) throws InterruptedException {
+
+        long time = System.currentTimeMillis() + 60000;
+        while (System.currentTimeMillis() < time) {
+            byte[] results = (byte[]) resultCache.get(id);
+            if (results != null) {
+                return results;
+            } else {
+                Thread.sleep(2500);
+            }
+        }
+        return null;
     }
 
     public void addResults(byte[] bytes, long id) {
-        final Object lock = locks.get(id);
-        if (lock != null) {
-            results.put(id, bytes);
-            synchronized (lock) {
-                lock.notify();
-            }
+        try {
+            resultCache.put(id, bytes);
+        } catch (Exception e) {
+            LogClass.error(e);
         }
     }
 }
