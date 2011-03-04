88490082c627096d09e8686068ee1755335623f6
Alan Baldwin <abaldwin@easy-insight.com>
2011-03-04 02:19:24 +0000

    woops

--- java/src/com/easyinsight/export/SeleniumLauncher.java
+++ java/src/com/easyinsight/export/SeleniumLauncher.java
@@ -20,7 +20,7 @@ import java.text.MessageFormat;
  */
 public class SeleniumLauncher {
 
-    public static final String OUTBOUND_QUEUE = "EISelenium";
+    public static final String OUTBOUND_QUEUE = "EISeleniumAlan";
 
     private static final String URL = "/app/selenium/selenium.jsp?userName={0}&password={1}&seleniumID={2}&dataSourceID={3}&reportID={4}&reportType={5}&width={6}&height={7}";
 
