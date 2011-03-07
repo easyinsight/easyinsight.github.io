dcc38e62dcc982f41d587434636555b66076ef2e
James Boe <jboe@easy-insight.com>
2011-03-07 21:28:09 +0000

    rollback of part

--- java/src/com/easyinsight/pipeline/DerivedGroupingComponent.java
+++ java/src/com/easyinsight/pipeline/DerivedGroupingComponent.java
@@ -1,14 +1,11 @@
 package com.easyinsight.pipeline;
 
-import com.easyinsight.analysis.*;
-import com.easyinsight.calculations.*;
-import com.easyinsight.calculations.generated.CalculationsLexer;
-import com.easyinsight.calculations.generated.CalculationsParser;
+import com.easyinsight.analysis.AnalysisCalculation;
+import com.easyinsight.analysis.DataResults;
+import com.easyinsight.analysis.DerivedAnalysisDimension;
+import com.easyinsight.analysis.IRow;
 import com.easyinsight.core.Value;
 import com.easyinsight.dataset.DataSet;
-import org.antlr.runtime.ANTLRStringStream;
-import org.antlr.runtime.CommonTokenStream;
-import org.antlr.runtime.RecognitionException;
 
 /**
  * User: jamesboe
@@ -24,29 +21,8 @@ public class DerivedGroupingComponent implements IComponent {
     }
 
     public DataSet apply(DataSet dataSet, PipelineData pipelineData) {
-        ICalculationTreeVisitor visitor;
-        CalculationsParser.expr_return ret;
-        CalculationsLexer lexer = new CalculationsLexer(new ANTLRStringStream(dimension.getDerivationCode()));
-        CommonTokenStream tokes = new CommonTokenStream();
-        tokes.setTokenSource(lexer);
-        CalculationsParser parser = new CalculationsParser(tokes);
-        parser.setTreeAdaptor(new NodeFactory());
-        try {
-            ret = parser.expr();
-        } catch (RecognitionException e) {
-            throw new RuntimeException(e);
-        }
-        CalculationTreeNode calculationTreeNode = (CalculationTreeNode) ret.getTree();
-        for (int i = 0; i < calculationTreeNode.getChildCount();i++) {
-            if (!(calculationTreeNode.getChild(i) instanceof CalculationTreeNode)) {
-                calculationTreeNode.deleteChild(i);
-                break;
-            }
-        }
-        visitor = new ResolverVisitor(pipelineData.getAllItems(), new FunctionFactory());
-        calculationTreeNode.accept(visitor);
         for (IRow row : dataSet.getRows()) {
-            Value value = dimension.calculate(row, calculationTreeNode);
+            Value value = dimension.calculate(row, pipelineData.getAllItems());
             row.addValue(dimension.createAggregateKey(), value);
         }
         pipelineData.getReportItems().add(dimension);
