02bc338f23241dc6298816b3bb54f51a7f6e4685
James Boe <jboe@easy-insight.com>
2011-04-06 23:36:14 +0000

    still trying

--- java/src/com/easyinsight/analysis/AnalysisDateDimension.java
+++ java/src/com/easyinsight/analysis/AnalysisDateDimension.java
@@ -152,7 +152,7 @@ public class AnalysisDateDimension extends AnalysisDimension {
         if (tempDate != null) {
             System.out.println("came in as " + tempDate);
             Calendar calendar = Calendar.getInstance();
-
+            //calendar.setTime(tempDate);
             if (timezoneShift) {
                 int time = insightRequestMetadata.getUtcOffset() / 60;
                 String string;
@@ -166,9 +166,8 @@ public class AnalysisDateDimension extends AnalysisDimension {
                 TimeZone timeZone = TimeZone.getTimeZone(string);
                 System.out.println("using " + timeZone.getID() + " against start date of " + tempDate);
                 calendar.setTimeZone(timeZone);
-                tempDate = new Date(System.currentTimeMillis() - insightRequestMetadata.getUtcOffset() * 60 * 1000);
             }
-            calendar.setTime(tempDate);
+            calendar.setTimeInMillis(tempDate.getTime());
             if (dateLevel <= WEEK_LEVEL || dateLevel == QUARTER_OF_YEAR_LEVEL) {
                 switch (dateLevel) {
                     case YEAR_LEVEL:
