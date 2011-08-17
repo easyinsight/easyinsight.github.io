87a34705d1dc7b9cba701cd119639b87ff4297a8
James Boe <jboe@easy-insight.com>
2011-08-17 03:49:55 +0000

    build fix

---
+++ java/src/com/easyinsight/users/BasicInfo.java
@@ -0,0 +1,27 @@
+package com.easyinsight.users;
+
+/**
+ * User: jamesboe
+ * Date: 8/16/11
+ * Time: 8:40 PM
+ */
+public class BasicInfo {
+    private String version;
+    private String prefix;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+}
