44f32232f2b7dcb9d5d3167774df2fccbda0525d
James Boe <jboe@easy-insight.com>
2011-08-20 22:21:54 +0000

    sql script update

--- java/sql/224.sql
+++ java/sql/224.sql
@@ -1,3 +1,3 @@
-alter table account add(subdomain varchar(255), subdomain_enabled tinyint(4), login_image bigint(20));
+alter table account add(subdomain varchar(255) default null, subdomain_enabled tinyint(4) not null default 0, login_image bigint(20) default null);
 alter table account add constraint account_ibfk_7 foreign key (login_image) references user_image(user_image_id);
 create unique index account_subdomain_idx on account(subdomain);
