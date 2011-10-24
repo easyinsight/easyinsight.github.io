24e99172c041e9c4a0ea8b525f24d7d60a66b792
James Boe <jboe@easy-insight.com>
2011-10-24 01:55:13 +0000

    fun with rollbacks...

--- java/src/com/easyinsight/pipeline/CalculationComponent.java
+++ java/src/com/easyinsight/pipeline/CalculationComponent.java
@@ -37,8 +37,70 @@ public class CalculationComponent implements IComponent {
         if (dataSet.getRows().size() == 0) {
             return dataSet;
         }
-        FieldCalculationLogic fieldCalculationLogic = new FieldCalculationLogic(analysisCalculation, dataSet);
-        fieldCalculationLogic.calculate(analysisCalculation.getCalculationString(), pipelineData.getReport(), pipelineData.getAllItems());
+        CalculationMetadata calculationMetadata = new CalculationMetadata();
+        calculationMetadata.setReport(pipelineData.getReport());
+        CalculationTreeNode calculationTreeNode;
+        ICalculationTreeVisitor visitor;
+        CalculationsParser.expr_return ret;
+        CalculationsLexer lexer = new CalculationsLexer(new ANTLRStringStream(analysisCalculation.getCalculationString()));
+        CommonTokenStream tokes = new CommonTokenStream();
+        tokes.setTokenSource(lexer);
+        Resolver r = new Resolver();
+
+        for (Key key : dataSet.getRow(0).getKeys()) {
+            r.addKey(key);
+        }
+        CalculationsParser parser = new CalculationsParser(tokes);
+        parser.setTreeAdaptor(new NodeFactory());
+        try {
+            ret = parser.expr();
+            calculationTreeNode = (CalculationTreeNode) ret.getTree();
+            for (int i = 0; i < calculationTreeNode.getChildCount();i++) {
+                if (!(calculationTreeNode.getChild(i) instanceof CalculationTreeNode)) {
+                    calculationTreeNode.deleteChild(i);
+                    break;
+                }
+            }
+            Map<String, List<AnalysisItem>> keyMap = new HashMap<String, List<AnalysisItem>>();
+            for (AnalysisItem analysisItem : pipelineData.getAllItems()) {
+                List<AnalysisItem> items = keyMap.get(analysisItem.getKey().toKeyString());
+                if (items == null) {
+                    items = new ArrayList<AnalysisItem>(1);
+                    keyMap.put(analysisItem.getKey().toKeyString(), items);
+                }
+                items.add(analysisItem);
+            }
+            Map<String, List<AnalysisItem>> displayMap = new HashMap<String, List<AnalysisItem>>();
+            for (AnalysisItem analysisItem : pipelineData.getAllItems()) {
+                List<AnalysisItem> items = displayMap.get(analysisItem.toDisplay());
+                if (items == null) {
+                    items = new ArrayList<AnalysisItem>(1);
+                    displayMap.put(analysisItem.toDisplay(), items);
+                }
+                items.add(analysisItem);
+            }
+            visitor = new ResolverVisitor(keyMap, displayMap, new FunctionFactory());
+            calculationTreeNode.accept(visitor);
+            for (IRow row : dataSet.getRows()) {
+                ICalculationTreeVisitor rowVisitor = new EvaluationVisitor(row, analysisCalculation, calculationMetadata);
+                calculationTreeNode.accept(rowVisitor);
+                Value result = rowVisitor.getResult();
+                if (result.type() == Value.EMPTY) {
+
+                } else if (result.type() == Value.NUMBER) {
+
+                } else {
+                    result = new NumericValue(result.toDouble());
+                }
+                row.addValue(analysisCalculation.createAggregateKey(), result);
+            }
+        } catch (FunctionException fe) {
+            throw new ReportException(new AnalysisItemFault(fe.getMessage(), analysisCalculation));
+        } catch (ReportException re) {
+            throw re;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage() + " in calculating " + analysisCalculation.getCalculationString(), e);
+        }
         pipelineData.getReportItems().add(analysisCalculation);
         return dataSet;
     }
--- java/src/com/easyinsight/pipeline/DerivedGroupingComponent.java
+++ java/src/com/easyinsight/pipeline/DerivedGroupingComponent.java
@@ -31,11 +31,64 @@ public class DerivedGroupingComponent implements IComponent {
         if (dataSet.getRows().size() == 0) {
             return dataSet;
         }
-        if (dataSet.getRows().size() == 0) {
-            return dataSet;
+        CalculationMetadata calculationMetadata = new CalculationMetadata();
+        calculationMetadata.setReport(pipelineData.getReport());
+        CalculationTreeNode calculationTreeNode;
+        ICalculationTreeVisitor visitor;
+        CalculationsParser.expr_return ret;
+        CalculationsLexer lexer = new CalculationsLexer(new ANTLRStringStream(dimension.getDerivationCode()));
+        CommonTokenStream tokes = new CommonTokenStream();
+        tokes.setTokenSource(lexer);
+        Resolver r = new Resolver();
+        for (Key key : dataSet.getRow(0).getKeys()) {
+            r.addKey(key);
         }
-        FieldCalculationLogic fieldCalculationLogic = new FieldCalculationLogic(dimension, dataSet);
-        fieldCalculationLogic.calculate(dimension.getDerivationCode(), pipelineData.getReport(), pipelineData.getAllItems());
+        CalculationsParser parser = new CalculationsParser(tokes);
+        parser.setTreeAdaptor(new NodeFactory());
+        try {
+            ret = parser.expr();
+            calculationTreeNode = (CalculationTreeNode) ret.getTree();
+            for (int i = 0; i < calculationTreeNode.getChildCount();i++) {
+                if (!(calculationTreeNode.getChild(i) instanceof CalculationTreeNode)) {
+                    calculationTreeNode.deleteChild(i);
+                    break;
+                }
+            }
+            Map<String, List<AnalysisItem>> keyMap = new HashMap<String, List<AnalysisItem>>();
+            for (AnalysisItem analysisItem : pipelineData.getAllItems()) {
+                List<AnalysisItem> items = keyMap.get(analysisItem.getKey().toKeyString());
+                if (items == null) {
+                    items = new ArrayList<AnalysisItem>(1);
+                    keyMap.put(analysisItem.getKey().toKeyString(), items);
+                }
+                items.add(analysisItem);
+            }
+            Map<String, List<AnalysisItem>> displayMap = new HashMap<String, List<AnalysisItem>>();
+            for (AnalysisItem analysisItem : pipelineData.getAllItems()) {
+                List<AnalysisItem> items = displayMap.get(analysisItem.toDisplay());
+                if (items == null) {
+                    items = new ArrayList<AnalysisItem>(1);
+                    displayMap.put(analysisItem.toDisplay(), items);
+                }
+                items.add(analysisItem);
+            }
+            visitor = new ResolverVisitor(keyMap, displayMap, new FunctionFactory());
+            calculationTreeNode.accept(visitor);
+
+            for (IRow row : dataSet.getRows()) {
+                ICalculationTreeVisitor rowVisitor = new EvaluationVisitor(row, dimension, calculationMetadata);
+                calculationTreeNode.accept(rowVisitor);
+                Value value = new StringValue(rowVisitor.getResult().toString());
+                row.addValue(dimension.createAggregateKey(), value);
+            }
+
+        } catch (FunctionException fe) {
+            throw new ReportException(new AnalysisItemFault(fe.getMessage() + " in the calculation of " + dimension.getDerivationCode() + ".", dimension));
+        } catch (ReportException re) {
+            throw re;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage() + " in calculating " + dimension.getDerivationCode(), e);
+        }
         pipelineData.getReportItems().add(dimension);
         return dataSet;
     }
