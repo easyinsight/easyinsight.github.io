3aada0b87b1a1028f5488a9fa1dac53f2240b952
James Boe <jboe@easy-insight.com>
2011-11-15 03:47:12 +0000

    grrrrrr

--- java/src/com/easyinsight/calculations/EvaluationVisitor.java
+++ java/src/com/easyinsight/calculations/EvaluationVisitor.java
@@ -213,9 +213,9 @@ public class EvaluationVisitor implements ICalculationTreeVisitor {
                 // TODO: Better handling of empty values in functions
                 if(subNode.getResult() instanceof EmptyValue) {
                     result = new EmptyValue();
-                    if (!(f instanceof FirstValueFunction) && !(f instanceof IfNotNull) && !(f instanceof GreaterThan) && !(f instanceof IsOnly) && !(f instanceof EqualTo)) {
+                    /*if (!(f instanceof FirstValueFunction) && !(f instanceof IfNotNull) && !(f instanceof GreaterThan) && !(f instanceof IsOnly) && !(f instanceof EqualTo)) {
                         return;
-                    }
+                    }*/
                     //return;
                 }
                 params.add(subNode.getResult());
