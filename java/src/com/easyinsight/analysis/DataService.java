30e74f9f1a961e2356658cf7f9666b8bbcce01b4
James Boe <jboe@easy-insight.com>
2011-12-04 15:59:01 +0000

    grr

--- java/src/com/easyinsight/analysis/DataService.java
+++ java/src/com/easyinsight/analysis/DataService.java
@@ -900,6 +900,7 @@ public class DataService {
                             if (analysisItem.hasType(AnalysisItemTypes.CALCULATION) && field.hasType(AnalysisItemTypes.CALCULATION)) {
                                 AnalysisCalculation sourceCalculation = (AnalysisCalculation) field;
                                 AnalysisCalculation targetCalculation = (AnalysisCalculation) analysisItem;
+                                System.out.println("updating calculation to " + sourceCalculation.getCalculationString());
                                 targetCalculation.setCalculationString(sourceCalculation.getCalculationString());
                             }
                         }
