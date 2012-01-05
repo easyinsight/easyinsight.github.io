2d63c3abae527882fa69f4abfb26cc93000931a9
James Boe <jboe@easy-insight.com>
2012-01-05 22:56:26 +0000

    argh

---
+++ java/src/com/easyinsight/datafeeds/basecamp/BaseCampTodoComments1To2.java
@@ -0,0 +1,38 @@
+package com.easyinsight.datafeeds.basecamp;
+
+import com.easyinsight.analysis.AnalysisDimension;
+import com.easyinsight.core.Key;
+import com.easyinsight.core.NamedKey;
+import com.easyinsight.database.EIConnection;
+import com.easyinsight.datafeeds.DataSourceMigration;
+import com.easyinsight.datafeeds.FeedDefinition;
+
+import java.util.Map;
+
+/**
+ * User: jamesboe
+ * Date: 9/17/11
+ * Time: 10:41 AM
+ */
+public class BaseCampTodoComments1To2 extends DataSourceMigration {
+    public BaseCampTodoComments1To2(FeedDefinition dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public void migrate(Map<String, Key> keys, EIConnection conn) throws Exception {
+        AnalysisDimension projectDimension = new AnalysisDimension(new NamedKey(BaseCampTodoCommentsSource.PROJECT_ID), true);
+        projectDimension.setHidden(true);
+        addAnalysisItem(projectDimension);
+    }
+
+    @Override
+    public int fromVersion() {
+        return 1;
+    }
+
+    @Override
+    public int toVersion() {
+        return 2;
+    }
+}
