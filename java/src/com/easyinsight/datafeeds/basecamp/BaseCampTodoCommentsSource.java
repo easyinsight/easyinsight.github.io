1ae4a4f4684d39278aeabb7cb39d0967460f3d59
James Boe <jboe@easy-insight.com>
2012-02-05 01:29:29 +0000

    wee

--- java/src/com/easyinsight/datafeeds/basecamp/BaseCampTodoCommentsSource.java
+++ java/src/com/easyinsight/datafeeds/basecamp/BaseCampTodoCommentsSource.java
@@ -3,14 +3,11 @@ package com.easyinsight.datafeeds.basecamp;
 import com.easyinsight.analysis.*;
 import com.easyinsight.core.Key;
 import com.easyinsight.database.EIConnection;
-import com.easyinsight.datafeeds.DataSourceMigration;
 import com.easyinsight.datafeeds.FeedDefinition;
 import com.easyinsight.datafeeds.FeedType;
 import com.easyinsight.dataset.DataSet;
 import com.easyinsight.security.SecurityUtil;
 import com.easyinsight.storage.IDataStorage;
-import com.easyinsight.storage.IWhere;
-import com.easyinsight.storage.StringWhere;
 import com.easyinsight.users.Token;
 import com.easyinsight.users.TokenStorage;
 import nu.xom.Builder;
@@ -22,10 +19,7 @@ import org.apache.ws.security.util.XmlSchemaDateFormat;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -35,14 +29,11 @@ import java.util.*;
  */
 public class BaseCampTodoCommentsSource extends BaseCampBaseSource {
 
-    public static final String XMLDATETIMEFORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-
     public static final String COMMENT_AUTHOR = "Todo Comment Author";
     public static final String TODO_ID = "Todo Parent ID";
     public static final String COMMENT_BODY = "Todo Comment Body";
     public static final String COMMENT_ID = "Todo Comment ID";
     public static final String COMMENT_CREATED_ON = "Todo Comment Created On";
-    public static final String PROJECT_ID = "Todo Comment Project ID";
 
     public static final String COUNT = "Todo Comment Count";
 
@@ -50,15 +41,9 @@ public class BaseCampTodoCommentsSource extends BaseCampBaseSource {
         setFeedName("Todo Comments");
     }
 
-    @Override
-    protected boolean clearsData(FeedDefinition parentSource) {
-        BaseCampCompositeSource source = (BaseCampCompositeSource) parentSource;
-        return !source.isIncrementalRefresh();
-    }
-
     @NotNull
     protected List<String> getKeys(FeedDefinition parentDefinition) {
-        return Arrays.asList(COUNT, COMMENT_AUTHOR, TODO_ID, COMMENT_BODY, COMMENT_ID, COMMENT_CREATED_ON, PROJECT_ID);
+        return Arrays.asList(COUNT, COMMENT_AUTHOR, TODO_ID, COMMENT_BODY, COMMENT_ID, COMMENT_CREATED_ON);
     }
 
     public FeedType getFeedType() {
@@ -69,9 +54,6 @@ public class BaseCampTodoCommentsSource extends BaseCampBaseSource {
         List<AnalysisItem> analysisItems = new ArrayList<AnalysisItem>();
         analysisItems.add(new AnalysisDimension(keys.get(COMMENT_AUTHOR), true));
         analysisItems.add(new AnalysisDimension(keys.get(COMMENT_ID), true));
-        AnalysisDimension projectDimension = new AnalysisDimension(keys.get(PROJECT_ID), true);
-        projectDimension.setHidden(true);
-        analysisItems.add(projectDimension);
         analysisItems.add(new AnalysisDimension(keys.get(TODO_ID), true));
         analysisItems.add(new AnalysisText(keys.get(COMMENT_BODY)));
         AnalysisDateDimension commentCreatedOnDim = new AnalysisDateDimension(keys.get(COMMENT_CREATED_ON), true, AnalysisDateDimension.DAY_LEVEL);
@@ -83,7 +65,6 @@ public class BaseCampTodoCommentsSource extends BaseCampBaseSource {
     public DataSet getDataSet(Map<String, Key> keys, Date now, FeedDefinition parentDefinition, IDataStorage IDataStorage, EIConnection conn, String callDataID, Date lastRefreshDate) {
         DataSet ds = new DataSet();
         BaseCampCompositeSource source = (BaseCampCompositeSource) parentDefinition;
-        DateFormat deadlineTimeFormat = new SimpleDateFormat(XMLDATETIMEFORMAT);
         if (source.isIncludeTodoComments()) {
 
             String url = source.getUrl();
@@ -102,25 +83,6 @@ public class BaseCampTodoCommentsSource extends BaseCampBaseSource {
                     Node curProject = projectNodes.get(i);
                     String projectName = queryField(curProject, "name/text()");
                     String projectID = queryField(curProject, "id/text()");
-                    String projectChangedOnString = queryField(curProject, "last-changed-on/text()");
-
-                    Date projectChangedAt;
-                    if (projectChangedOnString == null) {
-                        projectChangedAt = new Date();
-                    } else {
-                        projectChangedAt = deadlineTimeFormat.parse(projectChangedOnString);
-                    }
-
-                    if (lastRefreshDate == null) {
-                        lastRefreshDate = new Date(1);
-                    }
-                    long delta = lastRefreshDate.getTime() - projectChangedAt.getTime();
-
-                    long daysSinceChange = delta / (60 * 60 * 1000 * 24);
-
-                    if (source.isIncrementalRefresh() && daysSinceChange > 2) {
-                        continue;
-                    }
                     loadingProgress(i, projectNodes.size(), "Synchronizing with comments of " + projectName + "...", callDataID);
                     String projectStatus = queryField(curProject, "status/text()");
                     if (!source.isIncludeArchived() && "archived".equals(projectStatus)) {
@@ -162,7 +124,6 @@ public class BaseCampTodoCommentsSource extends BaseCampBaseSource {
                                             row.addValue(keys.get(TODO_ID), todoID);
                                             row.addValue(keys.get(COMMENT_AUTHOR), authorName);
                                             row.addValue(keys.get(COMMENT_BODY), body);
-                                            row.addValue(keys.get(PROJECT_ID), projectID);
                                             row.addValue(keys.get(COMMENT_CREATED_ON), createdDate);
                                             row.addValue(keys.get(COUNT), 1);
                                         }
@@ -171,12 +132,7 @@ public class BaseCampTodoCommentsSource extends BaseCampBaseSource {
                             }
                         }
                     }
-                    if (!source.isIncrementalRefresh() || lastRefreshDate == null || lastRefreshDate.getTime() < 100) {
-                        IDataStorage.insertData(ds);
-                    } else {
-                        StringWhere stringWhere = new StringWhere(keys.get(PROJECT_ID), projectID);
-                        IDataStorage.updateData(ds, Arrays.asList((IWhere) stringWhere));
-                    }
+                    IDataStorage.insertData(ds);
                     ds = new DataSet();
                 }
             } catch (ReportException re) {
@@ -188,18 +144,9 @@ public class BaseCampTodoCommentsSource extends BaseCampBaseSource {
         return null;
     }
 
-    @Override
-    protected String getUpdateKeyName() {
-        return PROJECT_ID;
-    }
-
-    @Override
-    public List<DataSourceMigration> getMigrations() {
-        return Arrays.asList((DataSourceMigration) new BaseCampTodoComments1To2(this));
-    }
 
     @Override
     public int getVersion() {
-        return 2;
+        return 1;
     }
 }
