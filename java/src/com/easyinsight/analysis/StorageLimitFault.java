70a71dc1dd2c4a5bd50f468e26df6e2405fb0700
James Boe <jboe@easy-insight.com>
2012-04-10 22:18:49 +0000

    build fix

--- java/src/com/easyinsight/analysis/StorageLimitFault.java
+++ java/src/com/easyinsight/analysis/StorageLimitFault.java
@@ -1,5 +1,9 @@
 package com.easyinsight.analysis;
 
+import com.easyinsight.users.DataSourceStats;
+
+import java.util.List;
+
 /**
  * User: jamesboe
  * Date: Nov 3, 2010
@@ -7,12 +11,22 @@ package com.easyinsight.analysis;
  */
 public class StorageLimitFault extends ReportFault {
     private String message;
+    private List<DataSourceStats> statsList;
 
     public StorageLimitFault() {
     }
 
-    public StorageLimitFault(String message) {
+    public StorageLimitFault(String message, List<DataSourceStats> statsList) {
         this.message = message;
+        this.statsList = statsList;
+    }
+
+    public List<DataSourceStats> getStatsList() {
+        return statsList;
+    }
+
+    public void setStatsList(List<DataSourceStats> statsList) {
+        this.statsList = statsList;
     }
 
     public String getMessage() {
