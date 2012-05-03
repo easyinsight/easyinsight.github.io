90279677bbbe831e75f3c24cff05a7c402afb3b4
James Boe <jboe@easy-insight.com>
2012-05-03 15:32:23 +0000

    fix

---
+++ java/src/com/easyinsight/datafeeds/UserThreadMutex.java
@@ -0,0 +1,39 @@
+package com.easyinsight.datafeeds;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+
+/**
+ * User: jamesboe
+ * Date: Apr 26, 2010
+ * Time: 10:54:19 AM
+ */
+public class UserThreadMutex {
+
+    private static final UserThreadMutex mutex = new UserThreadMutex();
+
+    private Map<Long, Semaphore> mutexMap = new HashMap<Long, Semaphore>();
+
+    public static UserThreadMutex mutex() {
+        return mutex;
+    }
+
+    public void acquire(long userID) throws InterruptedException {
+        Semaphore semaphore = mutexMap.get(userID);
+        if (semaphore == null) {
+            semaphore = new Semaphore(1);
+            mutexMap.put(userID, semaphore);
+            semaphore.acquire();
+        } else {
+            semaphore.acquire();
+        }
+    }
+
+    public void release(long userID) {
+        Semaphore semaphore = mutexMap.get(userID);
+        if (semaphore != null) {
+            semaphore.release();
+        }
+    }
+}
