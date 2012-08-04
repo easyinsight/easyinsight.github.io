e70a573d7a9b6d6891100d1536635b41c3d5b134
Alan Baldwin <abaldwin@easy-insight.com>
2012-08-04 22:13:26 +0000

    null owasp logger

---
+++ java/src/com/easyinsight/logging/OwaspLogger.java
@@ -0,0 +1,32 @@
+package com.easyinsight.logging;
+
+import org.owasp.csrfguard.log.ILogger;
+import org.owasp.csrfguard.log.LogLevel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alan
+ * Date: 8/4/12
+ * Time: 4:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OwaspLogger implements ILogger {
+    private static final long serialVersionUID = -7150989152365008214L;
+
+    public void log(String s) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void log(LogLevel logLevel, String s) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void log(Exception e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void log(LogLevel logLevel, Exception e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+}
