2be8c3ad00077d36d9049842f83d13443400c083
James Boe <jboe@easy-insight.com>
2012-08-24 20:32:55 +0000

    fixed diagram drillthroughs

--- java/src/com/easyinsight/analysis/definitions/WSDiagramDefinition.java
+++ java/src/com/easyinsight/analysis/definitions/WSDiagramDefinition.java
@@ -53,6 +53,6 @@ public class WSDiagramDefinition extends WSKPIDefinition {
     public String toHTML(String targetDiv, HTMLReportMetadata htmlReportMetadata) {
         String timezoneOffset = "timezoneOffset='+new Date().getTimezoneOffset()+'";
         String format = "$.getJSON(''/app/diagramChart?reportID={0}&{1}&''+ strParams, function(data) '{' window.drawDiagram(data, $(\"#{2}\"), ''{3}'') '}');";
-        return MessageFormat.format(format, getUrlKey(), timezoneOffset, targetDiv, getAnalysisID());
+        return MessageFormat.format(format, getUrlKey(), timezoneOffset, targetDiv, getUrlKey());
     }
 }
