3896e4fb5b619bf103e2482325f05523d19b7ec5
James Boe <jboe@easy-insight.com>
2012-08-29 23:13:58 +0000

    asdf

--- java/src/com/easyinsight/datafeeds/basecampnext/InitRetrieval.java
+++ java/src/com/easyinsight/datafeeds/basecampnext/InitRetrieval.java
@@ -20,12 +20,12 @@ public class InitRetrieval extends BasecampNextBaseSource {
     @NotNull
     @Override
     protected List<String> getKeys(FeedDefinition parentDefinition) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        throw new UnsupportedOperationException();
     }
 
     public List<BasecampNextAccount> getAccounts(BasecampNextCompositeSource parent) throws JSONException {
         JSONObject jsonObject = rawJSONRequestForObject("https://launchpad.37signals.com/authorization.json", parent);
-        Object accountsArrayObject = jsonObject.getJSONObject("accounts");
+        Object accountsArrayObject = jsonObject.getJSONArray("accounts");
         List<BasecampNextAccount> accounts = new ArrayList<BasecampNextAccount>();
         if (accountsArrayObject instanceof JSONArray) {
             JSONArray accountsArray = (JSONArray) accountsArrayObject;
