91011251a1f0fe41885cc992e77b2d2ff32afa53
James Boe <jboe@easy-insight.com>
2012-09-04 17:35:12 +0000

    hrm

--- java/src/com/easyinsight/calculations/GreaterThan.java
+++ java/src/com/easyinsight/calculations/GreaterThan.java
@@ -1,6 +1,9 @@
 package com.easyinsight.calculations;
 
+import com.easyinsight.core.DateValue;
 import com.easyinsight.core.Value;
+
+import java.util.Calendar;
 
 /**
  * User: jamesboe
@@ -11,11 +14,41 @@ public class GreaterThan extends Function {
     public Value evaluate() {
         Value compare1 = minusQuotes(getParameter(0));
         Value compare2 = minusQuotes(getParameter(1));
-        if (compare1.toDouble() > compare2.toDouble()) {
-            return minusQuotes(getParameter(2));
+        if (compare1.type() == Value.DATE || compare2.type() == Value.DATE) {
+            Calendar cal1 = Calendar.getInstance();
+            Calendar cal2 = Calendar.getInstance();
+
+            if (compare1.type() == Value.DATE) {
+                DateValue dateValue = (DateValue) compare1;
+                cal1.setTime(dateValue.getDate());
+            } else {
+                cal1.setTimeInMillis(compare1.toDouble().longValue());
+            }
+
+            if (compare2.type() == Value.DATE) {
+                DateValue dateValue = (DateValue) compare1;
+                cal2.setTime(dateValue.getDate());
+            } else {
+                cal2.setTimeInMillis(compare2.toDouble().longValue());
+            }
+
+            if (cal1.get(Calendar.YEAR) > cal2.get(Calendar.YEAR) ||
+                    (cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) && cal1.get(Calendar.DAY_OF_YEAR) > cal2.get(Calendar.DAY_OF_YEAR))) {
+                return minusQuotes(getParameter(2));
+            } else {
+                return minusQuotes(getParameter(3));
+            }
         } else {
-            return minusQuotes(getParameter(3));
+            if (compare1.toDouble() > compare2.toDouble()) {
+                return minusQuotes(getParameter(2));
+            } else {
+                return minusQuotes(getParameter(3));
+            }
         }
+        /*
+        if (cal1.get(Calendar.DAY_OF_YEAR) > cal2.get(Calendar.DAY_OF_YEAR) &&
+                    cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)) {
+         */
     }
 
     @Override
