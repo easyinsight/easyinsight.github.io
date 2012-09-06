bc0c02af51ba9dcde99388ce767eddeca7dd4f5e
Alan Baldwin <abaldwin@easy-insight.com>
2012-09-06 15:48:48 +0000

    new selenium runner

--- remoterunner/src/com/easyinsight/selenium/SeleniumRunner.java
+++ remoterunner/src/com/easyinsight/selenium/SeleniumRunner.java
@@ -6,7 +6,16 @@ import com.xerox.amazonws.sqs2.Message;
 import com.xerox.amazonws.sqs2.MessageQueue;
 import com.xerox.amazonws.sqs2.SQSException;
 import com.xerox.amazonws.sqs2.SQSUtils;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.openqa.selenium.server.SeleniumServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * User: jamesboe
@@ -15,6 +24,7 @@ import org.openqa.selenium.server.SeleniumServer;
  */
 public class SeleniumRunner {
 
+    private static Date lastDate = new Date();
     public static final String url = "https://www.easy-insight.com/";
 
     public static void main(String[] args) throws Exception {
@@ -22,14 +32,30 @@ public class SeleniumRunner {
         seleniumServer.start();
 
         boolean running = true;
-
         new Thread(new ReportListener(running)).start();
-        new Thread(new WholeFoodsRunner(running)).start();
-
+        Server server = new Server(5050);
+        server.setHandler(new LocalHandler());
+        server.start();
         while (running) {
             Thread.sleep(3000000);
         }
         //seleniumServer.stop();
+    }
+
+    private static class LocalHandler extends AbstractHandler {
+
+        public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException, ServletException {
+            System.out.println("Responding...");
+            Date curDate = new Date();
+            if(curDate.getTime() - lastDate.getTime() > (12 * 60 * 60 * 1000))
+                response.sendError(503);
+            else {
+                response.setStatus(200);
+                response.setContentType("text/plain");
+                response.getOutputStream().write("All ok!".getBytes());
+                response.getOutputStream().flush();
+            }
+        }
     }
 
     private static class ReportListener implements Runnable {
@@ -68,6 +94,7 @@ public class SeleniumRunner {
                                             // ignore
                                         }
                                         selenium.stop();
+                                        lastDate = new Date();
                                     } catch (Exception e) {
                                         e.printStackTrace();
                                     }
