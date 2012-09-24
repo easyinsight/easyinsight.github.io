50e637cff39db811e007bed24d954e2fed433e99
Alan Baldwin <abaldwin@easy-insight.com>
2012-09-24 02:45:37 +0000

    updated html report

--- java/src/com/easyinsight/analysis/FlatDateFilter.java
+++ java/src/com/easyinsight/analysis/FlatDateFilter.java
@@ -11,13 +11,13 @@ import java.util.*;
  * Time: 11:03 PM
  */
 @Entity
-@Table(name="flat_date_filter")
-@PrimaryKeyJoinColumn(name="filter_id")
+@Table(name = "flat_date_filter")
+@PrimaryKeyJoinColumn(name = "filter_id")
 public class FlatDateFilter extends FilterDefinition {
-    @Column(name="date_level")
+    @Column(name = "date_level")
     private int dateLevel;
 
-    @Column(name="selected_value")
+    @Column(name = "selected_value")
     private int value;
 
     @Transient
@@ -53,12 +53,12 @@ public class FlatDateFilter extends FilterDefinition {
     }
 
     @Override
-        public String toQuerySQL(String tableName) {
-            if(dateLevel == AnalysisDateDimension.MONTH_LEVEL)
-                return "month(" + getField().toKeySQL() + ") = (? + 1)";
-            else
-                return "year(" + getField().toKeySQL() + ") = ?";
-        }
+    public String toQuerySQL(String tableName) {
+        if (dateLevel == AnalysisDateDimension.MONTH_LEVEL)
+            return "month(" + getField().toKeySQL() + ") = (? + 1)";
+        else
+            return "year(" + getField().toKeySQL() + ") = ?";
+    }
 
     @Override
     public boolean validForQuery() {
@@ -74,30 +74,44 @@ public class FlatDateFilter extends FilterDefinition {
     @Override
     public String toHTML(FilterHTMLMetadata filterHTMLMetadata) {
         StringBuilder sb = new StringBuilder();
-        String filterName = "filter"+getFilterID();
-        String onChange = "updateFilter('filter" + getFilterID() + "','"+filterHTMLMetadata.getFilterKey()+"',"+filterHTMLMetadata.createOnChange()+")";
+        String filterName = "filter" + getFilterID();
+        String onChange = "updateFilter('filter" + getFilterID() + "','" + filterHTMLMetadata.getFilterKey() + "'," + filterHTMLMetadata.createOnChange() + ")";
         sb.append(label(true));
         sb.append("<select style=\"margin-left:5px;margin-top:5px;margin-right:5px\" id=\"").append(filterName).append("\" onchange=\"").append(onChange).append("\">");
-        AnalysisDateDimensionResultMetadata metadata = (AnalysisDateDimensionResultMetadata) new DataService().getAnalysisItemMetadata(filterHTMLMetadata.getDataSourceID(),
-                getField(), 0, 0, 0, filterHTMLMetadata.getReport());
-        Date earliestDate = metadata.getEarliestDate();
-        Date latestDate = metadata.getLatestDate();
-        Calendar cal = Calendar.getInstance();
+        if (dateLevel == AnalysisDateDimension.MONTH_LEVEL) {
+            List<String> months = Arrays.asList("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+            for(int i = 0;i < 12;i++) {
+                sb.append("<option value=\"");
+                sb.append(i);
+                sb.append("\"");
+                if(i == this.getValue())
+                    sb.append(" selected=\"selected\"");
+                sb.append(">");
+                sb.append(months.get(i));
+                sb.append("</option>");
+            }
+        } else {
+            AnalysisDateDimensionResultMetadata metadata = (AnalysisDateDimensionResultMetadata) new DataService().getAnalysisItemMetadata(filterHTMLMetadata.getDataSourceID(),
+                    getField(), 0, 0, 0, filterHTMLMetadata.getReport());
+            Date earliestDate = metadata.getEarliestDate();
+            Date latestDate = metadata.getLatestDate();
+            Calendar cal = Calendar.getInstance();
 
-        cal.setTime(earliestDate);
-        int startYear = cal.get(Calendar.YEAR);
-        cal.setTime(latestDate);
-        int endYear = cal.get(Calendar.YEAR);
-        List<String> stringList = new ArrayList<String>();
-        for (int i = startYear; i <= endYear; i++) {
-            stringList.add(String.valueOf(i));
-        }
+            cal.setTime(earliestDate);
+            int startYear = cal.get(Calendar.YEAR);
+            cal.setTime(latestDate);
+            int endYear = cal.get(Calendar.YEAR);
+            List<String> stringList = new ArrayList<String>();
+            for (int i = startYear; i <= endYear; i++) {
+                stringList.add(String.valueOf(i));
+            }
 
-        for (String value : stringList) {
-            if (value.equals(String.valueOf(value))) {
-                sb.append("<option selected=\"selected\">").append(value).append("</option>");
-            } else {
-                sb.append("<option>").append(value).append("</option>");
+            for (String value : stringList) {
+                if (value.equals(String.valueOf(value))) {
+                    sb.append("<option selected=\"selected\">").append(value).append("</option>");
+                } else {
+                    sb.append("<option>").append(value).append("</option>");
+                }
             }
         }
         sb.append("</select>");
