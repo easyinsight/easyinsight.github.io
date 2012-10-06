f76671eeb45fd785e2efe29daa3141df528f0503
James Boe <jboe@easy-insight.com>
2012-10-06 17:42:14 +0000

    lalalala

---
+++ java/src/com/easyinsight/analysis/ReportAccessException.java
@@ -0,0 +1,15 @@
+package com.easyinsight.analysis;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alan
+ * Date: 8/9/12
+ * Time: 9:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReportAccessException extends Exception {
+
+    public ReportAccessException() {
+        super();
+    }
+}
