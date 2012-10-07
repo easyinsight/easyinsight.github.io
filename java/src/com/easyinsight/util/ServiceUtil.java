5e48e763e3b03353c50e8be8afa8e50bcdbeb2b1
James Boe <jboe@easy-insight.com>
2012-10-07 18:27:50 +0000

    debug cleanup

--- java/src/com/easyinsight/util/ServiceUtil.java
+++ java/src/com/easyinsight/util/ServiceUtil.java
@@ -81,7 +81,7 @@ public class ServiceUtil {
     }
 
     public CallData getCallData(String callDataID) {
-        System.out.println("retrieving call data for " + callDataID);
+        //System.out.println("retrieving call data for " + callDataID);
         CallData callData = get().get(callDataID);
         if (callData == null) {
             return null;
