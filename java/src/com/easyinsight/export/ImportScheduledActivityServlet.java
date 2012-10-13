33eda291df91679863f451f478bec1959fc4848d
James Boe <jboe@easy-insight.com>
2012-10-13 15:52:36 +0000

    sql error on import fix

--- java/src/com/easyinsight/export/ImportScheduledActivityServlet.java
+++ java/src/com/easyinsight/export/ImportScheduledActivityServlet.java
@@ -5,10 +5,14 @@ import com.easyinsight.api.v3.ResponseInfo;
 import com.easyinsight.core.XMLImportMetadata;
 import com.easyinsight.database.EIConnection;
 import com.easyinsight.datafeeds.FeedStorage;
+import com.easyinsight.email.UserStub;
 import nu.xom.Document;
 import nu.xom.Element;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Iterator;
 
 /**
  * User: jamesboe
@@ -23,18 +27,38 @@ public class ImportScheduledActivityServlet extends APIServlet {
         XMLImportMetadata xmlImportMetadata = new XMLImportMetadata();
         xmlImportMetadata.setConn(conn);
 
+        PreparedStatement userExists = conn.prepareStatement("SELECT EMAIL FROM USER WHERE USER_ID = ?");
         if ("reportDelivery".equals(rootElement.getLocalName())) {
             xmlImportMetadata.setDataSource(new FeedStorage().getFeedDefinitionData(Long.parseLong(rootElement.getAttribute("dataSourceID").getValue())));
             ReportDelivery reportDelivery = new ReportDelivery();
             reportDelivery.fromXML(rootElement, xmlImportMetadata);
+            Iterator<UserStub> iter = reportDelivery.getUsers().iterator();
+            while (iter.hasNext()) {
+                UserStub userStub = iter.next();
+                userExists.setLong(1, userStub.getUserID());
+                ResultSet existsRS = userExists.executeQuery();
+                if (!existsRS.next()) {
+                    iter.remove();
+                }
+            }
             reportDelivery.save(conn, reportDelivery.getTimezoneOffset());
             reportDelivery.setup(conn);
         } else if ("multipleDelivery".equals(rootElement.getLocalName())) {
             GeneralDelivery generalDelivery = new GeneralDelivery();
             generalDelivery.fromXML(rootElement, xmlImportMetadata);
+            Iterator<UserStub> iter = generalDelivery.getUsers().iterator();
+            while (iter.hasNext()) {
+                UserStub userStub = iter.next();
+                userExists.setLong(1, userStub.getUserID());
+                ResultSet existsRS = userExists.executeQuery();
+                if (!existsRS.next()) {
+                    iter.remove();
+                }
+            }
             generalDelivery.save(conn, generalDelivery.getTimezoneOffset());
             generalDelivery.setup(conn);
         }
+        userExists.close();
         return new ResponseInfo(ResponseInfo.ALL_GOOD, "");
     }
 }
