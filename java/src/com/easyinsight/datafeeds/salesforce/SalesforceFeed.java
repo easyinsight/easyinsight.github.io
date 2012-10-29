9d693d4fccde38fba8ef78af506fb80d7f0f6762
James Boe <jboe@easy-insight.com>
2012-10-29 14:32:40 +0000

    salesforce fix

--- java/src/com/easyinsight/datafeeds/salesforce/SalesforceFeed.java
+++ java/src/com/easyinsight/datafeeds/salesforce/SalesforceFeed.java
@@ -222,7 +222,7 @@ public class SalesforceFeed extends Feed {
 
                 Nodes nextRecords = doc.query("/QueryResult/nextRecordsUrl/text()");
                 if (nextRecords.size() == 1) {
-                    url = nextRecords.get(0).getValue();
+                    url = salesforceBaseDataSource.getInstanceName() + nextRecords.get(0).getValue();
                     moreData = true;
                 } else {
                     moreData = false;
