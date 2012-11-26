e7729ce968786c3ec62b58a3dcd9553098fe3879
James Boe <jboe@easy-insight.com>
2012-11-26 18:09:19 +0000

    build fix

---
+++ java/src/com/easyinsight/html/HtmlConstants.java
@@ -0,0 +1,13 @@
+package com.easyinsight.html;
+
+/**
+ * User: jamesboe
+ * Date: 11/20/12
+ * Time: 1:21 PM
+ */
+public class HtmlConstants {
+    public static final int DATA_SOURCES_AND_REPORTS = 1;
+    public static final int ACCOUNT = 2;
+    public static final int WHATS_NEW = 3;
+    public static final int NONE = 4;
+}
