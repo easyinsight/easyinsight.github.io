4fef323b435bbecf3656e856d37d4b3bf4fa79ee
Alan Baldwin <abaldwin@easy-insight.com>
2012-12-03 21:37:18 +0000

    trend diagrams

--- java/src/com/easyinsight/analysis/definitions/WSTrendDefinition.java
+++ java/src/com/easyinsight/analysis/definitions/WSTrendDefinition.java
@@ -84,11 +84,12 @@ public class WSTrendDefinition extends WSKPIDefinition {
             ExportMetadata md = ExportService.createExportMetadata(SecurityUtil.getAccountID(false), conn, insightRequestMetadata);
             TrendDataResults results = DataService.getTrendDataResults(this, insightRequestMetadata, conn);
             for (TrendOutcome outcome : results.getTrendOutcomes()) {
+                TextValueExtension e = (TextValueExtension) outcome.getNow().getValueExtension();
                 String fontColor = ExportService.getFontColor(outcome);
                 double v = ((outcome.getNow().toDouble() / outcome.getHistorical().toDouble()) - 1.0) * 100.0;
                 Link l = outcome.getMeasure().defaultLink();
                 String clickEvent = "";
-                if(l != null && l instanceof DrillThrough) {
+                if (l != null && l instanceof DrillThrough) {
                     JSONObject j = new JSONObject();
                     j.put("embedded", "false");
                     j.put("id", l.getLinkID());
@@ -96,30 +97,64 @@ public class WSTrendDefinition extends WSKPIDefinition {
                     j.put("source", outcome.getMeasure().getAnalysisItemID());
                     clickEvent = "drillThroughParameterized(" + j.toString() + ")";
                 }
-                sb.append("<div style='width: 250px;height:100px;'><div style='float: right;padding-top: 20px; padding-bottom: 20px; width: 80px;'> <div style='color:");
-                sb.append(fontColor);
+                sb.append("<div style='width: 245px;height:100px;");
+                if ("horizontal".equals(getDirection()))
+                    sb.append("display:inline-block;");
+                sb.append("color:");
+                if (e != null) {
+                    sb.append(String.format("#%06X", (0xFFFFFF & e.getColor())));
+                } else {
+                    sb.append(fontColor);
+                }
                 sb.append(";'>");
-                sb.append("<img src='/app/assets/icons/32x32/");
-                sb.append(ExportService.getIconImage(outcome));
-                sb.append("' /> ");
-                FormattingConfiguration c = new FormattingConfiguration();
-                c.setFormattingType(FormattingConfiguration.PERCENTAGE);
+                sb.append("<div style='padding: 16px;'>");
+                TrendReportFieldExtension extension = ((TrendReportFieldExtension) outcome.getMeasure().getReportFieldExtension());
 
-                sb.append(c.createFormatter().format(v));
 
-                sb.append("</div><div>");
-                sb.append(outcome.getMeasure().getDisplayName());
-                sb.append("</div></div> <div style='font-size: 36px;font-weight: bold;padding-top: 34px;padding-bottom: 34px;color:");
-                sb.append(fontColor);
-                sb.append(";'> ");
-                if(!clickEvent.isEmpty()) {
+                if (extension.getDate() != null) {
+                    sb.append("<div style='float: right; width: 80px;font-size:");
+                    sb.append(getMinorFontSize());
+                    sb.append("px;'>");
+                    FormattingConfiguration c = new FormattingConfiguration();
+                    c.setFormattingType(FormattingConfiguration.PERCENTAGE);
+                    if (!clickEvent.isEmpty()) {
+                        sb.append("<a href='#' class='trendDrillthrough' onclick='");
+                        sb.append(clickEvent);
+                        sb.append("'>");
+                    }
+                    sb.append(c.createFormatter().format(v));
+                    if (!clickEvent.isEmpty()) {
+                        sb.append("</a>");
+                    }
+
+                    sb.append("</div>");
+                }
+                sb.append("<div style='font-size:");
+                sb.append(getMajorFontSize());
+                sb.append("px;text-align:center'> ");
+                if (!clickEvent.isEmpty()) {
                     sb.append("<a href='#' class='trendDrillthrough' onclick='");
                     sb.append(clickEvent);
                     sb.append("'>");
                 }
                 sb.append(ExportService.createValue(0, outcome.getMeasure(), outcome.getNow(), md.cal, md.currencySymbol, false));
 
-                if(!clickEvent.isEmpty()) {
+                if (!clickEvent.isEmpty()) {
+                    sb.append("</a>");
+                }
+
+                sb.append("</div></div>");
+                sb.append("<div style='text-align:center;color:#000000;font-size:");
+
+                sb.append(getMinorFontSize());
+                sb.append("px;'>");
+                if (!clickEvent.isEmpty()) {
+                    sb.append("<a href='#' class='trendDrillthrough' onclick='");
+                    sb.append(clickEvent);
+                    sb.append("'>");
+                }
+                sb.append(outcome.getMeasure().getDisplayName());
+                if (!clickEvent.isEmpty()) {
                     sb.append("</a>");
                 }
 
