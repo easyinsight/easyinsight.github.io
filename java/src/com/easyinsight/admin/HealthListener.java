058c560fbb3517c78675c54868508cb457f87434
Alan Baldwin <abaldwin@easy-insight.com>
2013-02-14 10:17:39 -0700

    i like sleep

--- java/src/com/easyinsight/admin/HealthListener.java
+++ java/src/com/easyinsight/admin/HealthListener.java
@@ -50,43 +50,32 @@ public class HealthListener implements Runnable {
                 String response = "All Good";
                 String code = SUCCESS;
                 {
-                    int activeConnections = Database.instance().getActiveConnections();
-                    int maxConnections = Database.instance().getMaxConnections();
-                    if (activeConnections == maxConnections) {
-                        response = "Core database pool is exhausted";
+                    EIConnection conn = Database.instance().getConnection();
+                    try {
+                        Statement stmt = conn.createStatement();
+                        stmt.executeQuery("SELECT 1");
+                    } catch (Exception e) {
+                        response = e.getMessage() + " on query of core database pool";
                         code = FAILURE;
-                    } else {
-                        EIConnection conn = Database.instance().getConnection();
-                        try {
-                            Statement stmt = conn.createStatement();
-                            stmt.executeQuery("SELECT 1");
-                        } catch (Exception e) {
-                            response = e.getMessage() + " on query of core database pool";
-                            code = FAILURE;
-                        } finally {
-                            Database.closeConnection(conn);
-                        }
+                    } finally {
+                        Database.closeConnection(conn);
                     }
+
                 }
                 for (Map.Entry<String, Database> storageEntry : DatabaseManager.instance().getDbMap().entrySet()) {
                     Database database = storageEntry.getValue();
-                    int activeConnections = database.getActiveConnections();
-                    int maxConnections = database.getMaxConnections();
-                    if (activeConnections == maxConnections) {
-                        response = "Database pool " + storageEntry.getKey() + " is exhausted";
+
+                    EIConnection conn = database.getConnection();
+                    try {
+                        Statement stmt = conn.createStatement();
+                        stmt.executeQuery("SELECT 1");
+                    } catch (Exception e) {
+                        response = e.getMessage() + " on query of " + storageEntry.getKey() + " database pool";
                         code = FAILURE;
-                    } else {
-                        EIConnection conn = database.getConnection();
-                        try {
-                            Statement stmt = conn.createStatement();
-                            stmt.executeQuery("SELECT 1");
-                        } catch (Exception e) {
-                            response = e.getMessage() + " on query of " + storageEntry.getKey() + " database pool";
-                            code = FAILURE;
-                        } finally {
-                            Database.closeConnection(conn);
-                        }
+                    } finally {
+                        Database.closeConnection(conn);
                     }
+
                 }
                 Status status = new Status();
                 status.setTime(System.currentTimeMillis());
@@ -109,8 +98,7 @@ public class HealthListener implements Runnable {
 
     private void initialSetup() {
         EIConnection conn = Database.instance().getConnection();
-        try
-        {
+        try {
             String host = InetAddress.getLocalHost().getHostName();
             PreparedStatement stmt = conn.prepareStatement("SELECT SERVER_ID FROM SERVER WHERE SERVER_HOST = ?");
             stmt.setString(1, host);
