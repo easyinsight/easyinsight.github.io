8e047c1bfaf9e7b9e00a5291adbca7714f685d04
James Boe <jboe@easy-insight.com>
2013-05-07 17:51:23 -0600

    added htmlWidth property to dashboardElement

--- java/src/com/easyinsight/dashboard/DashboardElement.java
+++ java/src/com/easyinsight/dashboard/DashboardElement.java
@@ -57,6 +57,8 @@ public abstract class DashboardElement implements Cloneable, Serializable {
     private int preferredWidth;
     private int preferredHeight;
 
+    private int htmlWidth;
+
     public JSONObject toJSON(FilterHTMLMetadata filterHTMLMetadata) throws JSONException {
         JSONObject jo = new JSONObject();
         if(getPreferredWidth() > 0)
@@ -65,6 +67,14 @@ public abstract class DashboardElement implements Cloneable, Serializable {
             jo.put("preferredHeight", getPreferredHeight());
 
         return jo;
+    }
+
+    public int getHtmlWidth() {
+        return htmlWidth;
+    }
+
+    public void setHtmlWidth(int htmlWidth) {
+        this.htmlWidth = htmlWidth;
     }
 
     public long getElementServerID() {
@@ -234,7 +244,7 @@ public abstract class DashboardElement implements Cloneable, Serializable {
     public void loadElement(long elementID, EIConnection conn) throws SQLException {
         PreparedStatement loadStmt = conn.prepareStatement("SELECT LABEL, FILTER_BORDER_STYLE, FILTER_BORDER_COLOR, filter_background_color, filter_background_alpha," +
                 "padding_left, padding_right, padding_top, padding_bottom, header_image_id, user_image.image_name, header_background_color, header_background_alpha," +
-                "preferred_width, preferred_height, dashboard_element_id, force_scrolling_off from dashboard_element " +
+                "preferred_width, preferred_height, dashboard_element_id, force_scrolling_off, html_width from dashboard_element " +
                 "left join user_image on dashboard_element.header_image_id = user_image.user_image_id where " +
                 "dashboard_element_id = ?");
         loadStmt.setLong(1, elementID);
@@ -268,14 +278,16 @@ public abstract class DashboardElement implements Cloneable, Serializable {
         setPreferredHeight(rs.getInt(i++));
         setElementID(rs.getLong(i++));
         setElementServerID(getElementID());
-        setForceScrollingOff(rs.getBoolean(i));
+        setForceScrollingOff(rs.getBoolean(i++));
+        setHtmlWidth(rs.getInt(i));
         loadStmt.close();
     }
 
     public long save(EIConnection conn) throws SQLException {
         PreparedStatement insertStmt = conn.prepareStatement("INSERT INTO DASHBOARD_ELEMENT (ELEMENT_TYPE, LABEL, filter_border_style, filter_border_color," +
                 "filter_background_color, filter_background_alpha, padding_left, padding_right, padding_top, padding_bottom, header_image_id," +
-                "header_background_color, header_background_alpha, preferred_width, preferred_height, force_scrolling_off) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                "header_background_color, header_background_alpha, preferred_width, preferred_height, force_scrolling_off, html_width) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                 PreparedStatement.RETURN_GENERATED_KEYS);
         int i = 1;
         insertStmt.setInt(i++, getType());
@@ -297,7 +309,8 @@ public abstract class DashboardElement implements Cloneable, Serializable {
         insertStmt.setDouble(i++, headerBackgroundAlpha);
         insertStmt.setInt(i++, preferredWidth);
         insertStmt.setInt(i++, preferredHeight);
-        insertStmt.setBoolean(i, forceScrollingOff);
+        insertStmt.setBoolean(i++, forceScrollingOff);
+        insertStmt.setInt(i, htmlWidth);
         insertStmt.execute();
         setElementID(Database.instance().getAutoGenKey(insertStmt));
         insertStmt.close();
