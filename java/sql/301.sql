f53aa2cc0c9604949781f845555850d0bb3f3702
James Boe <jboe@easy-insight.com>
2013-07-29 13:44:06 -0600

    build fix

--- java/sql/301.sql
+++ java/sql/301.sql
@@ -1,1 +1,7 @@
-alter table dashboard add image_full_header tinyint(4);
+create table system_settings (
+  system_settings_id bigint(20) auto_increment not null,
+  user_activity_semaphore_limit integer not null default 2,
+  primary key (system_settings_id)
+);
+
+alter table distributed_lock add lock_time datetime default null;
