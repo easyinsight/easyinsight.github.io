5a1fef400a9f7f701ad31e1c0c96da3580d7120b
James Boe <jboe@easy-insight.com>
2013-08-14 13:47:58 -0600

    toString() cleanup

--- java/src/com/easyinsight/analysis/Row.java
+++ java/src/com/easyinsight/analysis/Row.java
@@ -188,7 +188,7 @@ public class Row implements IRow, Serializable, Cloneable {
         }
         for (Key key : otherRow.dataSetKeys.getKeys()) {
             Value value = otherRow.getValue(key);
-            if (value.type() != Value.EMPTY && !"(Empty)".equals(value.toString())) {
+            if (value.type() != Value.EMPTY && (value.type() == Value.DATE || !"(Empty)".equals(value.toString()))) {
                 mergedRow.addValue(key, value);
             }
         }
