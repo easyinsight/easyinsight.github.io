d8178ab934b6cb3585b95b6dada8d0d65b735fe8
James Boe <jboe@easy-insight.com>
2013-08-14 15:01:24 -0600

    improving...

--- java/src/com/easyinsight/cache/MemCachedManager.java
+++ java/src/com/easyinsight/cache/MemCachedManager.java
@@ -1,18 +1,19 @@
 package com.easyinsight.cache;
 
 import com.easyinsight.config.ConfigLoader;
+import com.easyinsight.logging.LogClass;
 import net.spy.memcached.AddrUtil;
 import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.internal.OperationFuture;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 /**
- * Created with IntelliJ IDEA.
  * User: Alan
  * Date: 5/23/13
  * Time: 10:52 AM
- * To change this template use File | Settings | File Templates.
  */
 public class MemCachedManager {
 
@@ -28,5 +29,33 @@ public class MemCachedManager {
 
     public static MemcachedClient instance() {
         return client;
+    }
+
+    public static void add(String key, int expiration, Object o) {
+        try {
+            client.add(key, expiration, o);
+        } catch (Exception e) {
+            //LogClass.error(e);
+        }
+    }
+
+    @Nullable
+    public static Object get(String key) {
+        try {
+            return client.get(key);
+        } catch (Exception e) {
+            //LogClass.error(e);
+            return null;
+        }
+    }
+
+    @Nullable
+    public static OperationFuture<Boolean> delete(String key) {
+        try {
+            return client.delete(key);
+        } catch (Exception e) {
+            //LogClass.error(e);
+            return null;
+        }
     }
 }
