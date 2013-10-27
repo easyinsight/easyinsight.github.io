0afcdd01b3a0e08eb53a80f2749cda4aee89dec2
James Boe <jboe@easy-insight.com>
2013-10-27 10:58:54 -0600

    report caching

--- java/src/com/easyinsight/analysis/ReportCache.java
+++ java/src/com/easyinsight/analysis/ReportCache.java
@@ -1,14 +1,14 @@
 package com.easyinsight.analysis;
 
+import com.easyinsight.cache.MemCachedManager;
+import com.easyinsight.database.Database;
+import com.easyinsight.database.EIConnection;
 import com.easyinsight.dataset.DataSet;
 import com.easyinsight.logging.LogClass;
-import org.apache.jcs.JCS;
-import org.apache.jcs.access.exception.CacheException;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * User: jamesboe
@@ -17,8 +17,6 @@ import java.util.Map;
  */
 public class ReportCache {
     private static ReportCache instance;
-    private JCS reportCache = getCache("embeddedReports");
-    private JCS addonReportCache = getCache("addonReports");
 
     public static void initialize() {
         instance = new ReportCache();
@@ -28,79 +26,88 @@ public class ReportCache {
         return instance;
     }
 
-    private JCS getCache(String cacheName) {
-        try {
-            return JCS.getInstance(cacheName);
-        } catch (Exception e) {
-            LogClass.error(e);
-        }
-        return null;
-    }
-
     @Nullable
     public EmbeddedResults getResults(long dataSourceID, CacheKey cacheKey, int cacheTime) {
-        Map<CacheKey, EmbeddedResults> map = (Map<CacheKey, EmbeddedResults>) reportCache.get(dataSourceID);
-        if (map != null) {
-            EmbeddedResults results = map.get(cacheKey);
-            if (results != null) {
-                System.out.println("got time = " + new Date(results.getTime()));
-                if (cacheTime > 0 && System.currentTimeMillis() > (results.getTime() + (cacheTime * 1000 * 60))) {
-                    try {
-                        System.out.println("expired...");
-                        reportCache.remove(dataSourceID);
-                    } catch (CacheException e) {
-                    }
-                    return null;
-                }
-            }
-            return results;
-        }
-        return null;
+        return (EmbeddedResults) MemCachedManager.get("reportResults" + cacheKey.toString());
     }
 
-    public void storeReport(long dataSourceID, CacheKey cacheKey, EmbeddedResults results) throws CacheException {
-        Map<CacheKey, EmbeddedResults> map = (Map<CacheKey, EmbeddedResults>) reportCache.get(dataSourceID);
-        if (map == null) {
-            map = new HashMap<CacheKey, EmbeddedResults>();
-
+    public void storeReport(long dataSourceID, CacheKey cacheKey, EmbeddedResults results, int cacheTime) {
+        String cacheKeyString = cacheKey.toString();
+        EIConnection conn = Database.instance().getConnection();
+        try {
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO REPORT_CACHE (cache_key, data_source_id) VALUES (?, ?)");
+            ps.setString(1, cacheKeyString);
+            ps.setLong(2, dataSourceID);
+            ps.execute();
+            ps.close();
+        } catch (Exception e) {
+            LogClass.error(e);
+        } finally {
+            Database.closeConnection(conn);
         }
-        map.put(cacheKey, results);
-        reportCache.remove(dataSourceID);
-        reportCache.put(dataSourceID, map);
+        MemCachedManager.add(cacheKeyString, cacheTime * 60, results);
     }
 
     @Nullable
     public DataSet getAddonResults(long dataSourceID, CacheKey cacheKey, int cacheTime) {
-        Map<CacheKey, DataSet> map = (Map<CacheKey, DataSet>) addonReportCache.get(dataSourceID);
-        if (map != null) {
-            DataSet results = map.get(cacheKey);
-            if (results != null) {
-                if (cacheTime > 0 && System.currentTimeMillis() > (results.getTime() + (cacheTime * 1000 * 60))) {
-                    try {
-                        addonReportCache.remove(dataSourceID);
-                    } catch (CacheException e) {
-                    }
-                    return null;
-                }
+        return (DataSet) MemCachedManager.get("addonResults" + cacheKey.toString());
+    }
+
+    public void storeAddonReport(long dataSourceID, CacheKey cacheKey, DataSet results, int cacheTime) {
+        String cacheKeyString = cacheKey.toString();
+        EIConnection conn = Database.instance().getConnection();
+        try {
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO REPORT_CACHE (cache_key, data_source_id, report_id) VALUES (?, ?, ?)");
+            ps.setString(1, cacheKeyString);
+            ps.setLong(2, dataSourceID);
+            ps.setLong(3, cacheKey.getReportID());
+            ps.execute();
+            ps.close();
+        } catch (Exception e) {
+            LogClass.error(e);
+        } finally {
+            Database.closeConnection(conn);
+        }
+        MemCachedManager.add(cacheKeyString, cacheTime * 60, results);
+    }
+
+    public void flushResults(long dataSourceID) {
+        EIConnection conn = Database.instance().getConnection();
+        try {
+            PreparedStatement queryStmt = conn.prepareStatement("SELECT CACHE_KEY FROM REPORT_CACHE WHERE DATA_SOURCE_ID = ?");
+            queryStmt.setLong(1, dataSourceID);
+            ResultSet rs = queryStmt.executeQuery();
+            while (rs.next()) {
+                String cacheID = rs.getString(1);
+                MemCachedManager.delete(cacheID);
             }
-            return results;
+            PreparedStatement deleteStmt = conn.prepareStatement("DELETE FROM REPORT_CACHE WHERE DATA_SOURCE_ID = ?");
+            deleteStmt.setLong(1, dataSourceID);
+            deleteStmt.executeUpdate();
+        } catch (Exception e) {
+            LogClass.error(e);
+        } finally {
+            Database.closeConnection(conn);
         }
-        return null;
     }
 
-    public void storeAddonReport(long dataSourceID, CacheKey cacheKey, DataSet results) throws CacheException {
-        Map<CacheKey, DataSet> map = (Map<CacheKey, DataSet>) addonReportCache.get(dataSourceID);
-        if (map == null) {
-            map = new HashMap<CacheKey, DataSet>();
-
+    public void flushResultsForReport(long reportID) {
+        EIConnection conn = Database.instance().getConnection();
+        try {
+            PreparedStatement queryStmt = conn.prepareStatement("SELECT CACHE_KEY FROM REPORT_CACHE WHERE REPORT_ID = ?");
+            queryStmt.setLong(1, reportID);
+            ResultSet rs = queryStmt.executeQuery();
+            while (rs.next()) {
+                String cacheID = rs.getString(1);
+                MemCachedManager.delete(cacheID);
+            }
+            PreparedStatement deleteStmt = conn.prepareStatement("DELETE FROM REPORT_CACHE WHERE REPORT_ID = ?");
+            deleteStmt.setLong(1, reportID);
+            deleteStmt.executeUpdate();
+        } catch (Exception e) {
+            LogClass.error(e);
+        } finally {
+            Database.closeConnection(conn);
         }
-        map.put(cacheKey, results);
-        addonReportCache.remove(dataSourceID);
-        addonReportCache.put(dataSourceID, map);
-    }
-
-    public void flushResults(long dataSourceID) throws CacheException {
-        reportCache.remove(dataSourceID);
-        addonReportCache.remove(dataSourceID);
     }
 }
