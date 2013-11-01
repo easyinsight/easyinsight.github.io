c9e36a86ee0be6c23409c73eff333b55f2148951
Alan Baldwin <abaldwin@easy-insight.com>
2013-10-31 18:17:10 -0600

    html filters

---
+++ java/src/com/easyinsight/html/CreateConfigurationServlet.java
@@ -0,0 +1,23 @@
+package com.easyinsight.html;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alan
+ * Date: 10/31/13
+ * Time: 5:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CreateConfigurationServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
