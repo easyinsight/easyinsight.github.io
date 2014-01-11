6bd6de99ee3ffdd2ae335276354a114cf67cce01
James Boe <jboe@easy-insight.com>
2014-01-11 11:43:58 -0700

    joy

--- java/src/com/easyinsight/analysis/RollingFilterDefinition.java
+++ java/src/com/easyinsight/analysis/RollingFilterDefinition.java
@@ -229,9 +229,9 @@ public class RollingFilterDefinition extends FilterDefinition {
                     } else if (endDate != null) {
                         Calendar cal = Calendar.getInstance();
                         cal.setTime(endDate);
-                        cal.set(Calendar.HOUR_OF_DAY, 0);
-                        cal.set(Calendar.MINUTE, 0);
-                        cal.set(Calendar.SECOND, 0);
+                        cal.set(Calendar.HOUR_OF_DAY, 23);
+                        cal.set(Calendar.MINUTE, 59);
+                        cal.set(Calendar.SECOND, 59);
                         cal.set(Calendar.MILLISECOND, 0);
                         int time = insightRequestMetadata.getUtcOffset() / 60;
                         String string;
