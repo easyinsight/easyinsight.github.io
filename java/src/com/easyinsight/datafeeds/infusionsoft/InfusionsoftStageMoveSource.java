f908d91a1007536cd332b4e0e63ba18a3dd32c6c
James Boe <jboe@easy-insight.com>
2014-01-18 10:29:44 -0700

    infusionsoft fixes

--- java/src/com/easyinsight/datafeeds/infusionsoft/InfusionsoftStageMoveSource.java
+++ java/src/com/easyinsight/datafeeds/infusionsoft/InfusionsoftStageMoveSource.java
@@ -1,10 +1,9 @@
 package com.easyinsight.datafeeds.infusionsoft;
 
-import com.easyinsight.analysis.AnalysisDateDimension;
-import com.easyinsight.analysis.AnalysisDimension;
-import com.easyinsight.analysis.AnalysisItem;
-import com.easyinsight.analysis.ReportException;
+import com.easyinsight.analysis.*;
 import com.easyinsight.core.Key;
+import com.easyinsight.core.NamedKey;
+import com.easyinsight.core.Value;
 import com.easyinsight.database.EIConnection;
 import com.easyinsight.datafeeds.FeedDefinition;
 import com.easyinsight.datafeeds.FeedType;
@@ -48,17 +47,40 @@ public class InfusionsoftStageMoveSource extends InfusionsoftTableSource {
     public List<AnalysisItem> createAnalysisItems(Map<String, Key> keys, Connection conn, FeedDefinition parentDefinition) {
         List<AnalysisItem> analysisitems = new ArrayList<AnalysisItem>();
         analysisitems.add(new AnalysisDimension(keys.get(STAGE_MOVE_ID), "Stage Move ID"));
-        analysisitems.add(new AnalysisDimension(keys.get(OPPORTUNITY_ID), "Stage Order"));
+        analysisitems.add(new AnalysisDimension(keys.get(OPPORTUNITY_ID), "Opportunity ID"));
         analysisitems.add(new AnalysisDimension(keys.get(MOVE_TO_STAGE), "Move to Stage"));
         analysisitems.add(new AnalysisDimension(keys.get(MOVE_FROM_STAGE), "Move from Stage"));
         analysisitems.add(new AnalysisDateDimension(keys.get(MOVE_DATE), "Move Date", AnalysisDateDimension.MINUTE_LEVEL));
         return analysisitems;
     }
 
+    public List<AnalysisItem> createStageItems() {
+        List<AnalysisItem> analysisitems = new ArrayList<AnalysisItem>();
+        analysisitems.add(new AnalysisDimension(new NamedKey(InfusionsoftStageSource.STAGE_NAME), "Stage Name"));
+        analysisitems.add(new AnalysisDimension(new NamedKey(InfusionsoftStageSource.STAGE_ID), "Stage ID"));
+        return analysisitems;
+    }
+
     @Override
     public DataSet getDataSet(Map<String, Key> keys, Date now, FeedDefinition parentDefinition, IDataStorage IDataStorage, EIConnection conn, String callDataID, Date lastRefreshDate) throws ReportException {
         try {
-            return query("StageMove", createAnalysisItems(keys, conn, parentDefinition), (InfusionsoftCompositeSource) parentDefinition);
+            DataSet stages = query("Stage", createStageItems(), (InfusionsoftCompositeSource) parentDefinition);
+            Map<Value, Value> map = new HashMap<Value, Value>();
+            for (IRow row : stages.getRows()) {
+                Value stageID = row.getValue(new NamedKey(InfusionsoftStageSource.STAGE_ID));
+                Value stageName = row.getValue(new NamedKey(InfusionsoftStageSource.STAGE_NAME));
+                map.put(stageID, stageName);
+            }
+            DataSet stageMoveSet = query("StageMove", createAnalysisItems(keys, conn, parentDefinition), (InfusionsoftCompositeSource) parentDefinition);
+            for (IRow row : stageMoveSet.getRows()) {
+                Value moveTo = row.getValue(keys.get(MOVE_TO_STAGE));
+                Value moveToValue = map.get(moveTo);
+                Value moveFrom = row.getValue(keys.get(MOVE_FROM_STAGE));
+                Value moveFromValue = map.get(moveFrom);
+                row.addValue(keys.get(MOVE_TO_STAGE), moveToValue);
+                row.addValue(keys.get(MOVE_FROM_STAGE), moveFromValue);
+            }
+            return stageMoveSet;
         } catch (Exception e) {
             LogClass.error(e);
             throw new RuntimeException(e);
