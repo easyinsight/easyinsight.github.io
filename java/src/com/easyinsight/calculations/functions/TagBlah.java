7b3ac283a288c09a57530f11a792fd1ec4f28e42
James Boe <jboe@easy-insight.com>
2014-02-19 08:37:27 -0700

    tag method

---
+++ java/src/com/easyinsight/calculations/functions/TagBlah.java
@@ -0,0 +1,32 @@
+package com.easyinsight.calculations.functions;
+
+import com.easyinsight.calculations.Function;
+import com.easyinsight.core.*;
+
+/**
+ * User: jamesboe
+ * Date: 2/18/14
+ * Time: 3:56 PM
+ */
+public class TagBlah extends Function {
+    public Value evaluate() {
+        String string = params.get(0).toString();
+        String token = minusQuotes(1);
+        String component = minusQuotes(2);
+        String[] parts = string.split(",");
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i];
+            if (part.contains(token)) {
+                if (part.startsWith(component)) {
+                    String end = part.substring(part.indexOf(token) + 1).trim();
+                    return new StringValue(end);
+                }
+            }
+        }
+        return new EmptyValue();
+    }
+
+    public int getParameterCount() {
+        return 3;
+    }
+}
