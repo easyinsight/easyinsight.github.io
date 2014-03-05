3875dc607ef04c574efb1b41bd35d54d96c6f48d
James Boe <jboe@easy-insight.com>
2014-03-04 20:14:39 -0700

    solve360 fixes

--- java/src/com/easyinsight/datafeeds/solve360/Solve360CompositeSource.java
+++ java/src/com/easyinsight/datafeeds/solve360/Solve360CompositeSource.java
@@ -143,43 +143,56 @@ public class Solve360CompositeSource extends CompositeServerDataSource {
             companyMap = new HashMap<Integer, Company>();
             try {
                 HttpClient client = Solve360BaseSource.getHttpClient(userEmail, authKey);
-                Document doc = Solve360BaseSource.runRestRequest("https://secure.solve360.com/companies?layout=1", client, new Builder(), this);
-                Nodes responseNode = doc.query("/response");
-                Node response = responseNode.get(0);
-                DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd");
-                for (int i = 0; i < response.getChildCount(); i++) {
-                    Element companyNode = (Element) response.getChild(i);
-                    if (!"count".equals(companyNode.getLocalName()) && !"status".equals(companyNode.getLocalName())) {
-                        Company c = new Company();
-                        c.setCompanyId(Integer.parseInt(Solve360BaseSource.queryField(companyNode, "id/text()")));
-                        c.setName(Solve360BaseSource.queryField(companyNode, "name/text()"));
-                        c.setShippingAddress(Solve360BaseSource.queryField(companyNode, "shippingaddress/text()"));
-                        c.setBillingAddress(Solve360BaseSource.queryField(companyNode, "billingaddress/text()"));
-                        c.setMainAddress(Solve360BaseSource.queryField(companyNode, "mainaddress/text()"));
-                        c.setWebsite(Solve360BaseSource.queryField(companyNode, "website/text()"));
-                        c.setFaxNumber(Solve360BaseSource.queryField(companyNode, "fax/text()"));
-                        c.setCompanyPhone(Solve360BaseSource.queryField(companyNode, "phone/text()"));
-                        c.setResponsibleParty(Solve360BaseSource.queryField(companyNode, "assignedto/@cn"));
-                        c.setCompany(Solve360BaseSource.queryField(companyNode, "company/text()"));
-                        Map<Key, Object> customFieldValues = new HashMap<Key, Object>();
-                        for (AnalysisItem analysisItem : customCompanyFields) {
-                            String value = Solve360BaseSource.queryField(companyNode, analysisItem.getKey().toKeyString() + "/text()");
-                            if (analysisItem.hasType(AnalysisItemTypes.DATE_DIMENSION)) {
-                                if (value != null) {
-                                    try {
-                                        customFieldValues.put(analysisItem.getKey(), df2.parse(value));
-                                    } catch (ParseException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            } else {
-                                customFieldValues.put(analysisItem.getKey(), value);
-                            }
-                        }
-                        c.setCustomFieldValues(customFieldValues);
-                        companyMap.put(c.getCompanyId(), c);
+                int page = 0;
+                int count;
+                do {
+                    count = 0;
+                    Document doc;
+                    if (page == 0) {
+                        doc = Solve360BaseSource.runRestRequest("https://secure.solve360.com/companies?layout=1&limit=1000&start=1", client, new Builder(), this);
+                    } else {
+                        doc = Solve360BaseSource.runRestRequest("https://secure.solve360.com/companies?layout=1&limit=1000&start=" + (page * 1000), client, new Builder(), this);
                     }
-                }
+
+                    Nodes responseNode = doc.query("/response");
+                    Node response = responseNode.get(0);
+                    DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd");
+                    for (int i = 0; i < response.getChildCount(); i++) {
+                        Element companyNode = (Element) response.getChild(i);
+                        if (!"count".equals(companyNode.getLocalName()) && !"status".equals(companyNode.getLocalName())) {
+                            count++;
+                            Company c = new Company();
+                            c.setCompanyId(Integer.parseInt(Solve360BaseSource.queryField(companyNode, "id/text()")));
+                            c.setName(Solve360BaseSource.queryField(companyNode, "name/text()"));
+                            c.setShippingAddress(Solve360BaseSource.queryField(companyNode, "shippingaddress/text()"));
+                            c.setBillingAddress(Solve360BaseSource.queryField(companyNode, "billingaddress/text()"));
+                            c.setMainAddress(Solve360BaseSource.queryField(companyNode, "mainaddress/text()"));
+                            c.setWebsite(Solve360BaseSource.queryField(companyNode, "website/text()"));
+                            c.setFaxNumber(Solve360BaseSource.queryField(companyNode, "fax/text()"));
+                            c.setCompanyPhone(Solve360BaseSource.queryField(companyNode, "phone/text()"));
+                            c.setResponsibleParty(Solve360BaseSource.queryField(companyNode, "assignedto/@cn"));
+                            c.setCompany(Solve360BaseSource.queryField(companyNode, "company/text()"));
+                            Map<Key, Object> customFieldValues = new HashMap<Key, Object>();
+                            for (AnalysisItem analysisItem : customCompanyFields) {
+                                String value = Solve360BaseSource.queryField(companyNode, analysisItem.getKey().toKeyString() + "/text()");
+                                if (analysisItem.hasType(AnalysisItemTypes.DATE_DIMENSION)) {
+                                    if (value != null) {
+                                        try {
+                                            customFieldValues.put(analysisItem.getKey(), df2.parse(value));
+                                        } catch (ParseException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                } else {
+                                    customFieldValues.put(analysisItem.getKey(), value);
+                                }
+                            }
+                            c.setCustomFieldValues(customFieldValues);
+                            companyMap.put(c.getCompanyId(), c);
+                        }
+                        page++;
+                    }
+                } while (count == 1000);
             } catch (ParsingException e) {
                 LogClass.error(e);
             }
@@ -194,51 +207,65 @@ public class Solve360CompositeSource extends CompositeServerDataSource {
             contactMap = new HashMap<Integer, Contact>();
             try {
                 HttpClient client = Solve360BaseSource.getHttpClient(userEmail, authKey);
-                Document doc = Solve360BaseSource.runRestRequest("https://secure.solve360.com/contacts?layout=1", client, new Builder(), this);
-                Nodes responseNode = doc.query("/response");
-                Node response = responseNode.get(0);
-                DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd");
-                for (int i = 0; i < response.getChildCount(); i++) {
-                    Element contactNode = (Element) response.getChild(i);
-                    if (!"count".equals(contactNode.getLocalName()) && !"status".equals(contactNode.getLocalName())) {
-                        Contact c = new Contact();
-                        String s = Solve360BaseSource.queryField(contactNode, "id/text()");
-                        c.setId(Integer.parseInt(s));
-                        c.setName(Solve360BaseSource.queryField(contactNode, "name/text()"));
-                        c.setTitle(Solve360BaseSource.queryField(contactNode, "jobtitle/text()"));
-                        c.setBusinessAddress(Solve360BaseSource.queryField(contactNode, "businessaddress/text()"));
-                        c.setBusinessEmail(Solve360BaseSource.queryField(contactNode, "businessemail/text()"));
-                        c.setBusinessFax(Solve360BaseSource.queryField(contactNode, "businessfax/text()"));
-                        c.setBusinessPhoneDirect(Solve360BaseSource.queryField(contactNode, "businessphonedirect/text()"));
-                        c.setBusinessPhoneExt(Solve360BaseSource.queryField(contactNode, "businessphoneextension/text()"));
-                        c.setBusinessPhoneMain(Solve360BaseSource.queryField(contactNode, "businessphonemain/text()"));
-                        c.setCellphone(Solve360BaseSource.queryField(contactNode, "cellularphone/text()"));
-                        c.setCompany(Solve360BaseSource.queryField(contactNode, "company/text()"));
-                        c.setOtherEmail(Solve360BaseSource.queryField(contactNode, "otheremail/text()"));
-                        c.setPersonalEmail(Solve360BaseSource.queryField(contactNode, "personalemail/text()"));
-                        c.setWebsite(Solve360BaseSource.queryField(contactNode, "website/text()"));
-                        c.setHomeAddress(Solve360BaseSource.queryField(contactNode, "homeaddress/text()"));
-                        c.setHomePhone(Solve360BaseSource.queryField(contactNode, "homephone/text()"));
-                        c.setResponsibleUser(Solve360BaseSource.queryField(contactNode, "assignedto/@cn"));
-                        Map<Key, Object> customFieldValues = new HashMap<Key, Object>();
-                        for (AnalysisItem analysisItem : customContactFields) {
-                            String value = Solve360BaseSource.queryField(contactNode, analysisItem.getKey().toKeyString() + "/text()");
-                            if (analysisItem.hasType(AnalysisItemTypes.DATE_DIMENSION)) {
-                                if (value != null) {
-                                    try {
-                                        customFieldValues.put(analysisItem.getKey(), df2.parse(value));
-                                    } catch (ParseException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            } else {
-                                customFieldValues.put(analysisItem.getKey(), value);
-                            }
-                        }
-                        c.setCustomFieldValues(customFieldValues);
-                        contactMap.put(c.getId(), c);
+                Builder builder = new Builder();
+                int count;
+                int page = 0;
+                do {
+                    count = 0;
+                    Document doc;
+                    if (page == 0) {
+                        doc = Solve360BaseSource.runRestRequest("https://secure.solve360.com/contacts?layout=1&limit=1000", client, builder, this);
+                    } else {
+                        doc = Solve360BaseSource.runRestRequest("https://secure.solve360.com/contacts?layout=1&limit=1000&start=" + (page * 1000), client, builder, this);
                     }
-                }
+                    Nodes responseNode = doc.query("/response");
+                    Node response = responseNode.get(0);
+                    DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd");
+                    for (int i = 0; i < response.getChildCount(); i++) {
+                        Element contactNode = (Element) response.getChild(i);
+                        if (!"count".equals(contactNode.getLocalName()) && !"status".equals(contactNode.getLocalName())) {
+                            count++;
+                            Contact c = new Contact();
+                            String s = Solve360BaseSource.queryField(contactNode, "id/text()");
+                            c.setId(Integer.parseInt(s));
+                            c.setName(Solve360BaseSource.queryField(contactNode, "name/text()"));
+                            c.setTitle(Solve360BaseSource.queryField(contactNode, "jobtitle/text()"));
+                            c.setBusinessAddress(Solve360BaseSource.queryField(contactNode, "businessaddress/text()"));
+                            c.setBusinessEmail(Solve360BaseSource.queryField(contactNode, "businessemail/text()"));
+                            c.setBusinessFax(Solve360BaseSource.queryField(contactNode, "businessfax/text()"));
+                            c.setBusinessPhoneDirect(Solve360BaseSource.queryField(contactNode, "businessphonedirect/text()"));
+                            c.setBusinessPhoneExt(Solve360BaseSource.queryField(contactNode, "businessphoneextension/text()"));
+                            c.setBusinessPhoneMain(Solve360BaseSource.queryField(contactNode, "businessphonemain/text()"));
+                            c.setCellphone(Solve360BaseSource.queryField(contactNode, "cellularphone/text()"));
+                            c.setCompany(Solve360BaseSource.queryField(contactNode, "company/text()"));
+                            c.setOtherEmail(Solve360BaseSource.queryField(contactNode, "otheremail/text()"));
+                            c.setPersonalEmail(Solve360BaseSource.queryField(contactNode, "personalemail/text()"));
+                            c.setWebsite(Solve360BaseSource.queryField(contactNode, "website/text()"));
+                            c.setHomeAddress(Solve360BaseSource.queryField(contactNode, "homeaddress/text()"));
+                            c.setHomePhone(Solve360BaseSource.queryField(contactNode, "homephone/text()"));
+                            c.setResponsibleUser(Solve360BaseSource.queryField(contactNode, "assignedto/@cn"));
+                            Map<Key, Object> customFieldValues = new HashMap<Key, Object>();
+                            for (AnalysisItem analysisItem : customContactFields) {
+                                String value = Solve360BaseSource.queryField(contactNode, analysisItem.getKey().toKeyString() + "/text()");
+                                if (analysisItem.hasType(AnalysisItemTypes.DATE_DIMENSION)) {
+                                    if (value != null) {
+                                        try {
+                                            customFieldValues.put(analysisItem.getKey(), df2.parse(value));
+                                        } catch (ParseException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                } else {
+                                    customFieldValues.put(analysisItem.getKey(), value);
+                                }
+                            }
+                            c.setCustomFieldValues(customFieldValues);
+                            contactMap.put(c.getId(), c);
+                        }
+
+                    }
+                    page++;
+                } while (count == 1000);
             } catch (ParsingException e) {
                 LogClass.error(e);
             }
