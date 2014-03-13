bf44c7624fdffd1a22b5fbbc1e08f27fc4495cdc
James Boe <jboe@easy-insight.com>
2014-03-12 18:18:20 -0600

    cleanup

---
+++ java/sql/337.sql
@@ -0,0 +1,6 @@
+alter table diagram_report drop filter_name;
+alter table diagram_report drop day_window;
+alter table trend_grid_report drop filter_name;
+alter table trend_grid_report drop day_window;
+alter table trend_report drop filter_name;
+alter table trend_report drop day_window;
