1d476ddee002d2cfdc80f1a77ab073321d34007a
James Boe <jboe@easy-insight.com>
2014-03-15 08:41:58 -0600

    debug

--- java/src/com/easyinsight/analysis/Trend.java
+++ java/src/com/easyinsight/analysis/Trend.java
@@ -59,6 +59,9 @@ public class Trend {
                 TrendOutcome trendOutcome = entry.getValue();
                 if (trendOutcome.getNow() == null) {
                     trendOutcome.setNow(new EmptyValue());
+                    System.out.println("null now value");
+                } else {
+                    System.out.println("now value = " + trendOutcome.getNow().toDouble());
                 }
                 if (trendOutcome.getHistorical() == null) {
                     trendOutcome.setHistorical(new EmptyValue());
