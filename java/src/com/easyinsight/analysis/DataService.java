5e5ee23555ed0eac8594041390bb4e76af6f7a0a
James Boe <jboe@easy-insight.com>
2014-03-30 11:57:45 -0600

    wtf

--- java/src/com/easyinsight/analysis/DataService.java
+++ java/src/com/easyinsight/analysis/DataService.java
@@ -1372,9 +1372,14 @@ public class DataService {
                 AnalysisDateDimension date = (AnalysisDateDimension) wsytdDefinition.getTimeDimension();
                 date.setDateLevel(AnalysisDateDimension.MONTH_FLAT);
             }
-
+            if (customFilters != null) {
+                for (FilterDefinition filter : customFilters) {
+                    System.out.println("\tusing filter " + filter.getFilterName());
+                }
+            }
             ReportRetrieval reportRetrievalNow = ReportRetrieval.reportView(insightRequestMetadata, wsytdDefinition, conn, customFilters, drillthroughFilters);
             DataSet nowSet = reportRetrievalNow.getPipeline().toDataSet(reportRetrievalNow.getDataSet());
+            System.out.println("now set had size = " + nowSet.getRows().size());
             YTDStuff ytdStuff = YTDUtil.getYTDStuff(wsytdDefinition, nowSet, insightRequestMetadata, conn, reportRetrievalNow.getPipeline().getPipelineData(),
                     reportRetrievalNow.getPipeline().getPipelineData().getAllRequestedItems());
             Map<String, Object> additionalProperties = new HashMap<String, Object>();
