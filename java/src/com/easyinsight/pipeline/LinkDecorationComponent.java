9ca2d6572fe412480940827fa2c2dfa4ae508905
James Boe <jboe@easy-insight.com>
2014-04-19 18:12:06 -0600

    joyyyyy

--- java/src/com/easyinsight/pipeline/LinkDecorationComponent.java
+++ java/src/com/easyinsight/pipeline/LinkDecorationComponent.java
@@ -23,19 +23,12 @@ public class LinkDecorationComponent implements IComponent {
                         String url = link.generateLink(row, pipelineData.getDataSourceProperties(), pipelineData.getAllItems());
                         if (url != null) {
                             Value value = row.getValue(analysisItem.createAggregateKey());
-                            try {
-                                if (pipelineData.getInsightRequestMetadata().isLogReport()) {
-                                    System.out.println("Assigning " + url + " to " + value.toString() + " with qn " + analysisItem.qualifiedName());
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
                             Map<String, String> links = value.getLinks();
                             if (links == null) {
                                 links = new HashMap<String, String>(1);
                                 value.setLinks(links);
                             }
-                            links.put(analysisItem.qualifiedName(), url);
+                            links.put(analysisItem.toDisplay(), url);
                         }
                     }
                 }
