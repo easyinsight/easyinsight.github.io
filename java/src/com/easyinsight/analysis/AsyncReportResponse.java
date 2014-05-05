406d236816fdcc861349d04beec494862ac87605
James Boe <jboe@easy-insight.com>
2014-05-05 14:37:29 -0600

    build fix

---
+++ java/src/com/easyinsight/analysis/AsyncReportResponse.java
@@ -0,0 +1,18 @@
+package com.easyinsight.analysis;
+
+/**
+ * User: jamesboe
+ * Date: 4/22/14
+ * Time: 9:40 AM
+ */
+public class AsyncReportResponse {
+    private String callDataID;
+
+    public String getCallDataID() {
+        return callDataID;
+    }
+
+    public void setCallDataID(String callDataID) {
+        this.callDataID = callDataID;
+    }
+}
