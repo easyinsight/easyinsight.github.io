dc6c0f3f2ce77b05c92ed47b00f0d841a7677aa7
James Boe <jboe@easy-insight.com>
2014-06-03 21:11:15 -0600

    verbiage cleanup

--- java/src/com/easyinsight/analysis/definitions/WSVerticalListDefinition.java
+++ java/src/com/easyinsight/analysis/definitions/WSVerticalListDefinition.java
@@ -139,7 +139,7 @@ public class WSVerticalListDefinition extends WSAnalysisDefinition {
         return measures;
     }
 
-    public void argh() {
+    public void handleFieldExtensions() {
         Map<String, List<AnalysisItem>> map = new LinkedHashMap<String, List<AnalysisItem>>();
         boolean firstPH = true;
         for (AnalysisItem field : measures) {
