0db982765df6a9f7d2d881723a12e0439779de7f
James Boe <jboe@easy-insight.com>
2014-06-27 10:48:21 -0600

    servlet tweak

--- java/src/com/easyinsight/html/PieChartServlet.java
+++ java/src/com/easyinsight/html/PieChartServlet.java
@@ -3,15 +3,20 @@ package com.easyinsight.html;
 import com.easyinsight.analysis.*;
 
 import com.easyinsight.analysis.definitions.WSPieChartDefinition;
+import com.easyinsight.core.Value;
 import com.easyinsight.database.EIConnection;
 import com.easyinsight.dataset.DataSet;
 import com.easyinsight.export.ExportMetadata;
+import com.easyinsight.export.ExportService;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * User: jamesboe
@@ -53,15 +58,44 @@ public class PieChartServlet extends HtmlServlet {
 
         List<String> colors = chart.createMultiColors();
 
+
+
+
+
         JSONArray points = new JSONArray();
         int i = 0;
         AnalysisItem measureItem = measures.get(i);
+
+        double sum = 0;
+
+        Map<IRow, Double> percentOfTotals = new HashMap<>();
+        for (IRow row : dataSet.getRows()) {
+            sum += row.getValue(measureItem).toDouble();
+        }
+
+        for (IRow row : dataSet.getRows()) {
+            percentOfTotals.put(row, row.getValue(measureItem).toDouble() / sum * 100);
+        }
+
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMaximumFractionDigits(1);
+        nf.setMinimumFractionDigits(1);
+
         for (IRow row : dataSet.getRows()) {
             String x = row.getValue(xAxisItem).toString();
             JSONObject point = new JSONObject();
-            point.put("value", row.getValue(measureItem).toDouble());
+            Value value = row.getValue(measureItem);
+            point.put("value", value.toDouble());
             point.put("label", x);
+            point.put("total", sum);
+            point.put("percent", nf.format(percentOfTotals.get(row)));
             String color = colors.get(i % colors.size());
+            if (value.getValueExtension() != null && value.getValueExtension() instanceof TextValueExtension) {
+                TextValueExtension textValueExtension = (TextValueExtension) value.getValueExtension();
+                if (textValueExtension.getColor() > 0) {
+                    color = ExportService.createHexString(textValueExtension.getColor());
+                }
+            }
             point.put("color", color);
             points.put(point);
             i++;
