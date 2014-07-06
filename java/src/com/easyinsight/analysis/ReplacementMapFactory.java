0c635d8e22bc270a90ce81c22f0f026b3f92a7d8
James Boe <jboe@easy-insight.com>
2014-07-06 11:03:26 -0600

    template copy tweaks

---
+++ java/src/com/easyinsight/analysis/ReplacementMapFactory.java
@@ -0,0 +1,12 @@
+package com.easyinsight.analysis;
+
+/**
+ * User: jamesboe
+ * Date: 7/4/14
+ * Time: 9:58 AM
+ */
+public class ReplacementMapFactory {
+    public ReplacementMap createMap() {
+        return new ReplacementMap();
+    }
+}
