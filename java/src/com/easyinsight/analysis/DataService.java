ad14c80103a5b62bc1b1201f41ed45b4284ba229
James Boe <jboe@easy-insight.com>
2014-07-22 12:00:19 -0600

    trend tweak

--- java/src/com/easyinsight/analysis/DataService.java
+++ java/src/com/easyinsight/analysis/DataService.java
@@ -1730,6 +1730,12 @@ public class DataService {
             allItems.addAll(analysisDefinition.getAddedItems());
         }
 
+        Map<AnalysisItem, Integer> positionMap = new HashMap<>();
+        int position = 0;
+        for (AnalysisItem analysisItem : analysisDefinition.getMeasures()) {
+            positionMap.put(analysisItem, position++);
+        }
+
         for (AnalysisItem analysisItem : analysisDefinition.getMeasures()) {
 
             if (nowFilter != null) {
@@ -1750,24 +1756,31 @@ public class DataService {
                         }
                     }
                 }
-                if (date == null) {
+                /*if (date == null) {
                     for (AnalysisItem item : allItems) {
                         if (item.hasType(AnalysisItemTypes.DATE_DIMENSION)) {
                             date = (AnalysisDateDimension) item;
                             break;
                         }
                     }
-                }
+                }*/
                 if (date == null) {
-                    throw new ReportException(new GenericReportFault("You need to choose a date for " + analysisItem.toDisplay() + " for trend analysis to work."));
+                    List<AnalysisMeasure> measures = trendMap.get("");
+                    if (measures == null) {
+                        measures = new ArrayList<AnalysisMeasure>();
+                        trendMap.put("", measures);
+                    }
+                    measures.add((AnalysisMeasure) analysisItem);
+                    //throw new ReportException(new GenericReportFault("You need to choose a date for " + analysisItem.toDisplay() + " for trend analysis to work."));
+                } else {
+                    dateMap.put(date.toDisplay(), date);
+                    List<AnalysisMeasure> measures = trendMap.get(date.toDisplay());
+                    if (measures == null) {
+                        measures = new ArrayList<AnalysisMeasure>();
+                        trendMap.put(date.toDisplay(), measures);
+                    }
+                    measures.add((AnalysisMeasure) analysisItem);
                 }
-                dateMap.put(date.toDisplay(), date);
-                List<AnalysisMeasure> measures = trendMap.get(date.toDisplay());
-                if (measures == null) {
-                    measures = new ArrayList<AnalysisMeasure>();
-                    trendMap.put(date.toDisplay(), measures);
-                }
-                measures.add((AnalysisMeasure) analysisItem);
             } else {
                 List<AnalysisMeasure> measures = trendMap.get("");
                 if (measures == null) {
@@ -1790,7 +1803,7 @@ public class DataService {
             if (analysisDefinition.getGroupings() != null) {
                 columns.addAll(analysisDefinition.getGroupings());
             }
-            if (nowFilter != null) {
+            if (nowFilter != null && !"".equals(key)) {
                 List<FilterDefinition> filters = new ArrayList<FilterDefinition>();
                 for (FilterDefinition filter : analysisDefinition.getFilterDefinitions()) {
                     if (filter != nowFilter && filter != previousFilter) {
@@ -1807,7 +1820,13 @@ public class DataService {
                 filters.add(nowFilterClone);
                 tempReport.setFilterDefinitions(filters);
             } else {
-                tempReport.setFilterDefinitions(analysisDefinition.getFilterDefinitions());
+                List<FilterDefinition> filters = new ArrayList<FilterDefinition>();
+                for (FilterDefinition filter : analysisDefinition.getFilterDefinitions()) {
+                    if (filter != nowFilter && filter != previousFilter) {
+                        filters.add(filter);
+                    }
+                }
+                tempReport.setFilterDefinitions(filters);
             }
             tempReport.setColumns(columns);
             tempReport.setDataFeedID(analysisDefinition.getDataFeedID());
@@ -1858,7 +1877,7 @@ public class DataService {
                 }
             }
             DataSet pastSet;
-            if (previousFilter != null) {
+            if (previousFilter != null && !"".equals(key)) {
 
                 List<FilterDefinition> filters = new ArrayList<FilterDefinition>();
                 for (FilterDefinition filter : analysisDefinition.getFilterDefinitions()) {
@@ -1906,6 +1925,13 @@ public class DataService {
             }
             trendOutcomes.addAll(new Trend().calculateTrends(measures, analysisDefinition.getGroupings(), nowSet, pastSet));
         }
+        Collections.sort(trendOutcomes, new Comparator<TrendOutcome>() {
+
+            @Override
+            public int compare(TrendOutcome o1, TrendOutcome o2) {
+                return positionMap.get(o1.getMeasure()).compareTo(positionMap.get(o2.getMeasure()));
+            }
+        });
         return new TrendResult(new ArrayList<TrendOutcome>(trendOutcomes), dataSourceInfo, nowString, previousString);
     }
 
