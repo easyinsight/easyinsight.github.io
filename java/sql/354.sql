053e566fdf14ecee5eafbbc5b8d9c678e0fafa00
James Boe <jboe@easy-insight.com>
2014-08-05 15:07:21 -0600

    sql tweak

---
+++ java/sql/354.sql
@@ -0,0 +1,2 @@
+alter table text_report_field_extension add fixed_sort varchar(20) not null default 'Default';
+alter table text_report_field_extension add fixed_sort_order integer not null default 0;
