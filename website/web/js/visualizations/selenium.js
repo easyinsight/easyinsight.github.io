405e91dc798022a52d6805ab4ae76d5a4c1e327e
James Boe <jboe@easy-insight.com>
2014-08-09 10:05:39 -0600

    wee

--- website/web/js/visualizations/selenium.js
+++ website/web/js/visualizations/selenium.js
@@ -13,20 +13,22 @@ var afterRefresh = function (selector, id) {
     return function () {
         selector.hide();
         if (typeof(id) != "undefined") {
-            console.log("before timeout = " + id);
-            var svg = document.getElementById("d3Div" + id);
-            var h = svg.height();
-            var w = svg.width();
+            setTimeout(function () {
+                console.log("before timeout = d3Div" + id);
+                var svg = $("#d3Div" + id);
+                var h = svg.height();
+                var w = svg.width();
 
 
-            canvg("d3Canvas" + id, svg.html());
-            var canvas = document.getElementById("d3Canvas" + id);
-            var img = canvas.toDataURL("image/png");
-            $.ajax({
-                url: '/app/uploadExportImage?id=' + seleniumJSON["seleniumID"] + "&height=" + h + "&width=" + w,
-                data: {imgBase64: img},
-                type: "POST"
-            });
+                canvg("d3Canvas" + id, svg.html());
+                var canvas = document.getElementById("d3Canvas" + id);
+                var img = canvas.toDataURL("image/png");
+                $.ajax({
+                    url: '/app/uploadExportImage?id=' + seleniumJSON["seleniumID"] + "&height=" + h + "&width=" + w,
+                    data: {imgBase64: img},
+                    type: "POST"
+                });
+            }, 5000);
         }
     }
 };
