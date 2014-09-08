8914aeb6c757a59e84220740bb88ea3abe7a6b4a
James Boe <jboe@easy-insight.com>
2014-09-08 14:21:38 -0600

    fixes

--- java/src/com/easyinsight/datafeeds/batchbook2/Batchbook2CompositeSource.java
+++ java/src/com/easyinsight/datafeeds/batchbook2/Batchbook2CompositeSource.java
@@ -31,7 +31,7 @@ public class Batchbook2CompositeSource extends CompositeServerDataSource {
 
     public void configureFactory(HTMLConnectionFactory factory) {
         factory.addField("Batchbook URL", "url", "Your Batchbook URL is the browser URL you normally use to connect to Batchbook. For example, if you access Batchbook as yourcompanyname.batchbook.com, put yourcompanyname in as the Batchbook URL.");
-        factory.addField("Batchbook API Authentication Token:", "token", "You can find the token on your Batchbook page under My Info - API Token.");
+        factory.addField("Batchbook API Authentication Token:", "token", "You can find the token on your Batchbook page under Your Account - Integration.");
         factory.type(HTMLConnectionFactory.TYPE_BASIC_AUTH);
     }
 
