ef349d1776c1aa101c6c3e0e23424f37b821dfd7
James Boe <jboe@easy-insight.com>
2014-09-30 10:35:13 -0600

    joy

--- java/src/com/easyinsight/users/SetupReactivationServlet.java
+++ java/src/com/easyinsight/users/SetupReactivationServlet.java
@@ -1,9 +1,12 @@
 package com.easyinsight.users;
 
+import com.easyinsight.api.v3.JSONServlet;
+import com.easyinsight.api.v3.ResponseInfo;
 import com.easyinsight.database.Database;
 import com.easyinsight.database.EIConnection;
 import com.easyinsight.logging.LogClass;
 import com.easyinsight.security.SecurityUtil;
+import net.minidev.json.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +19,7 @@ import java.io.IOException;
  * Date: 9/16/14
  * Time: 10:38 AM
  */
-public class SetupReactivationServlet extends HttpServlet {
+public class SetupReactivationServlet extends JSONServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         SecurityUtil.populateThreadLocalFromSession(req);
@@ -39,5 +42,14 @@ public class SetupReactivationServlet extends HttpServlet {
         } finally {
             SecurityUtil.clearThreadLocal();
         }
+    }
+
+    @Override
+    protected ResponseInfo processJSON(JSONObject jsonObject, EIConnection conn, HttpServletRequest request) throws Exception {
+        SecurityUtil.authorizeAccountTier(Account.ADMINISTRATOR);
+        long accountID = Long.parseLong(request.getParameter("id"));
+        String bucket = request.getParameter("bucket");
+        new ReactivationAccount().generate(accountID, conn, bucket);
+        return new ResponseInfo(ResponseInfo.ALL_GOOD, "");
     }
 }
