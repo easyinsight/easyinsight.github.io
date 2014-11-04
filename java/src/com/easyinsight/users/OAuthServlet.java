ecbb3e341add3874991b2524138a3ef19fb26c00
James Boe <jboe@easy-insight.com>
2014-11-04 12:49:53 -0700

    grr

--- java/src/com/easyinsight/users/OAuthServlet.java
+++ java/src/com/easyinsight/users/OAuthServlet.java
@@ -86,7 +86,7 @@ public class OAuthServlet extends HttpServlet {
                 if(ConfigLoader.instance().isProduction()) {
                     redirectURL = "https://www.easy-insight.com";
                 } else {
-                    redirectURL = "https://localhost:4443";
+                    redirectURL = "https://j8staging.easy-insight.com";
                 }
 
                 redirectURL = redirectURL + "/app/googleAppsUserList.jsp";
@@ -118,7 +118,7 @@ public class OAuthServlet extends HttpServlet {
                         if (ConfigLoader.instance().isProduction()) {
                             redirectURL = "https://www.easy-insight.com/app/#connectionConfig=" + feedDefinition.getApiKey();
                         } else {
-                            redirectURL = "https://localhost:4443/app/#connectionConfig=" + feedDefinition.getApiKey();
+                            redirectURL = "https://j8staging/app/#connectionConfig=" + feedDefinition.getApiKey();
                         }
                     } else if (redirectType == TokenService.HTML_SETUP) {
                         redirectURL = RedirectUtil.getURL(req, "/app/html/dataSources/"+ feedDefinition.getApiKey() + "/createConnection");
