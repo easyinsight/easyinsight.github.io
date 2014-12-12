74343d8e680707872a52784359c8e365281a8485
Alan Baldwin <abaldwin@easy-insight.com>
2014-12-12 13:05:14 -0700

    watchdog wtf

--- java/src/com/easyinsight/servlet/EIContextListener.java
+++ java/src/com/easyinsight/servlet/EIContextListener.java
@@ -42,18 +42,6 @@ public class EIContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
             LogClass.info("Starting the core Easy Insight server...");
-            final List<byte[]> segments = new ArrayList<byte[]>(64000);
-
-                    final int size = 6400000;
-
-                    for (int i = 0; i < 1000000000; i++) {
-                        segments.add(new byte[size * i]);
-                    }
-
-                    for (final byte[] data : segments) {
-                        final int dataSize = data.length;
-                        System.out.printf("Segment [size=%s]\n", dataSize);
-                    }
 
             if (Database.instance() == null) {
                 SecurityUtil.setSecurityProvider(new DefaultSecurityProvider());
