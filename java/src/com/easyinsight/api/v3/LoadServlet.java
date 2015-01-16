f44c53a63fb325f24596d531cf7526605db145f8
James Boe <jboe@easy-insight.com>
2015-01-16 14:06:15 -0700

    more tweaks...

--- java/src/com/easyinsight/api/v3/LoadServlet.java
+++ java/src/com/easyinsight/api/v3/LoadServlet.java
@@ -3,7 +3,11 @@ package com.easyinsight.api.v3;
 import com.easyinsight.analysis.AsyncReport;
 import com.easyinsight.database.Database;
 import com.easyinsight.database.EIConnection;
+import com.easyinsight.export.DeliveryScheduledTask;
 import com.easyinsight.logging.LogClass;
+import com.easyinsight.scheduler.DataSourceScheduledTask;
+import com.easyinsight.scheduler.ScheduledTask;
+import org.hibernate.Session;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -42,23 +46,28 @@ public class LoadServlet extends HttpServlet {
         private String userEmail;
         private String accountName;
         private String dataSourceName;
+        private long taskID;
 
-        Activity(String name, long userID, String userEmail, String accountName, String dataSourceName) {
+        Activity(String name, long userID, String userEmail, String accountName, String dataSourceName, long taskID) {
             this.name = name;
             this.userID = userID;
             this.userEmail = userEmail;
             this.accountName = accountName;
             this.dataSourceName = dataSourceName;
+            this.taskID = taskID;
         }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<Integer, Server> loadMap = new HashMap<>();
+        int unassigned = 0;
         EIConnection conn = Database.instance().getConnection();
         try {
 
-            Map<Integer, Server> loadMap = new HashMap<>();
-            PreparedStatement stmt = conn.prepareStatement("SELECT server_id, server_host, data_source_listener, database_listener, report_listener, healthy, last_healthy_time FROM " +
+
+            PreparedStatement stmt = conn.prepareStatement("SELECT server_id, server_host, data_source_listener, database_listener, " +
+                    "report_listener, healthy, last_healthy_time FROM " +
                     "server WHERE enabled = ?");
             stmt.setBoolean(1, true);
             ResultSet rs = stmt.executeQuery();
@@ -73,7 +82,8 @@ public class LoadServlet extends HttpServlet {
                 loadMap.put(rs.getInt(1), server);
             }
             stmt.close();
-            PreparedStatement workStmt = conn.prepareStatement("SELECT async_report_request_id, assigned_server, request_state, user_id, request_type, data_source_id FROM " +
+            PreparedStatement workStmt = conn.prepareStatement("SELECT async_report_request_id, assigned_server, request_state, user_id, request_type, " +
+                    "data_source_id, cache_source_id, task_id FROM " +
                     "async_report_request WHERE request_state = ? OR request_state = ? OR request_state = ?");
             workStmt.setInt(1, AsyncReport.WAITING_ASSIGN);
             workStmt.setInt(2, AsyncReport.ASSIGNED);
@@ -84,7 +94,7 @@ public class LoadServlet extends HttpServlet {
             PreparedStatement dsStmt = conn.prepareStatement("SELECT data_feed.feed_name, data_feed.feed_type FROM data_feed WHERE data_feed_id = ?");
 
 
-            int unassigned = 0;
+
             List<Long> waitingForAssign = new ArrayList<>(1);
             while (workRS.next()) {
                 long requestID = workRS.getLong(1);
@@ -95,6 +105,8 @@ public class LoadServlet extends HttpServlet {
                 long userID = workRS.getLong(4);
                 int requestType = workRS.getInt(5);
                 long dataSourceID = workRS.getLong(6);
+                long cacheID = workRS.getLong(7);
+                long taskID = workRS.getLong(8);
                 if (serverAssigned) {
                     Server server = loadMap.get(assignedServer);
                     if (server != null && requestState == AsyncReport.ASSIGNED) {
@@ -112,6 +124,18 @@ public class LoadServlet extends HttpServlet {
                             activityName = "Report end user run";
                         } else if (requestType == AsyncReport.REPORT_EDITOR) {
                             activityName = "Report editor run";
+                        } else if (requestType == AsyncReport.SCHEDULED_TASK) {
+                            activityName = "Scheduled task";
+                        } else if (requestType == AsyncReport.CACHE_REBUILD) {
+                            activityName = "Cache rebuild";
+                        } else if (requestType == AsyncReport.FILE_UPLOAD_ANALYZE) {
+                            activityName = "Analyzing flat file create";
+                        } else if (requestType == AsyncReport.FILE_UPLOAD_CREATE) {
+                            activityName = "Creating source from flat file";
+                        }  else if (requestType == AsyncReport.FILE_REPLACE_UPLOAD) {
+                            activityName = "Analyzing flat file update";
+                        } else if (requestType == AsyncReport.FILE_REPLACE_ANALYZE) {
+                            activityName = "Updating source from flat file";
                         }
                         String email = null;
                         String name = null;
@@ -130,44 +154,76 @@ public class LoadServlet extends HttpServlet {
                             dataSourceName = dataSetRS.getString(1);
                         }
 
-                        server.currentActivities.add(new Activity(activityName, userID, email, name, dataSourceName));
+                        server.currentActivities.add(new Activity(activityName, userID, email, name, dataSourceName, taskID));
                     }
                 } else {
                     unassigned++;
                 }
             }
             workStmt.close();
-
-            if (unassigned > 0) {
-                resp.getOutputStream().write((unassigned + " unassigned requests waiting for processing...\r\n\r\n").getBytes());
-            }
-
-            for (Server server : loadMap.values()) {
-                resp.getOutputStream().write((server.host + "\r\n").getBytes());
-                resp.getOutputStream().write(("\tDatabase Listener: " + server.databaseListener + "\r\n").getBytes());
-                resp.getOutputStream().write(("\tReport and Data Source Listener: " + server.reportListener + "\r\n").getBytes());
-                resp.getOutputStream().write(("\tAssigned: " + server.assigned + "\r\n").getBytes());
-                resp.getOutputStream().write(("\tIn Progress: " + server.inProgress + "\r\n").getBytes());
-                resp.getOutputStream().write(("\tHealthy: " + server.healthy + "\r\n").getBytes());
-                resp.getOutputStream().write(("\tLast Healthy Time: " + server.lastHealthyTime + "\r\n").getBytes());
-                resp.getOutputStream().write(("\r\n").getBytes());
-
-                if (server.currentActivities.size() > 0) {
-                    resp.getOutputStream().write(("\r\n\tCurrent Server Work:\r\n").getBytes());
-                    for (Activity activity : server.currentActivities) {
-                        if ("Data source refresh".equals(activity.name)) {
-                            resp.getOutputStream().write(("\t\tRefresh of " + activity.dataSourceName + " by user " + activity.userEmail + " on " + activity.accountName + "\r\n").getBytes());
-                        } else {
-                            resp.getOutputStream().write(("\t\t" + activity.name + " by user " + activity.userEmail + " on " + activity.accountName + "\r\n").getBytes());
-                        }
-                    }
-                }
-            }
-            resp.getOutputStream().flush();
         } catch (Exception e) {
             LogClass.error(e);
         } finally {
             Database.closeConnection(conn);
         }
+
+        if (unassigned > 0) {
+            resp.getOutputStream().write((unassigned + " unassigned requests waiting for processing...\r\n\r\n").getBytes());
+        }
+
+        for (Server server : loadMap.values()) {
+            resp.getOutputStream().write((server.host + "\r\n").getBytes());
+            resp.getOutputStream().write(("\tDatabase Listener: " + server.databaseListener + "\r\n").getBytes());
+            resp.getOutputStream().write(("\tReport and Data Source Listener: " + server.reportListener + "\r\n").getBytes());
+            resp.getOutputStream().write(("\tAssigned: " + server.assigned + "\r\n").getBytes());
+            resp.getOutputStream().write(("\tIn Progress: " + server.inProgress + "\r\n").getBytes());
+            resp.getOutputStream().write(("\tHealthy: " + server.healthy + "\r\n").getBytes());
+            resp.getOutputStream().write(("\tLast Healthy Time: " + server.lastHealthyTime + "\r\n").getBytes());
+            resp.getOutputStream().write(("\r\n").getBytes());
+
+            if (server.currentActivities.size() > 0) {
+                resp.getOutputStream().write(("\r\n\tCurrent Server Work:\r\n").getBytes());
+                for (Activity activity : server.currentActivities) {
+                    if (activity.taskID > 0) {
+                        conn = Database.instance().getConnection();
+                        Session session = Database.instance().createSession(conn);
+                        try {
+                            PreparedStatement dsStmt = conn.prepareStatement("SELECT data_feed.feed_name, data_feed.feed_type FROM data_feed WHERE data_feed_id = ?");
+                            List tasks = session.createQuery("from ScheduledTask where scheduledTaskID = ?").setLong(0, activity.taskID).list();
+                            if (tasks.size() > 0) {
+                                ScheduledTask scheduledTask = (ScheduledTask) tasks.get(0);
+                                if (scheduledTask instanceof DataSourceScheduledTask) {
+                                    DataSourceScheduledTask dataSourceScheduledTask = (DataSourceScheduledTask) scheduledTask;
+                                    long dataSourceID = dataSourceScheduledTask.getDataSourceID();
+                                    dsStmt.setLong(1, dataSourceID);
+                                    ResultSet rs = dsStmt.executeQuery();
+                                    if (rs.next()) {
+                                        String dataSourceName = rs.getString(1);
+                                        resp.getOutputStream().write(("\t\tScheduled refresh of " + dataSourceName + "\r\n").getBytes());
+                                    } else {
+                                        resp.getOutputStream().write(("\t\tOrphan data source refresh\r\n").getBytes());
+                                    }
+                                } else {
+                                    resp.getOutputStream().write(("\t\t" + scheduledTask.getClass().getName()).getBytes());
+                                }
+                            } else {
+                                resp.getOutputStream().write(("\t\tOrphan task, shouldn't really be seeing this...").getBytes());
+                            }
+                            dsStmt.close();
+                        } catch (Exception e) {
+                            LogClass.error(e);
+                        } finally {
+                            session.close();
+                            Database.closeConnection(conn);
+                        }
+                    } else if ("Data source refresh".equals(activity.name)) {
+                        resp.getOutputStream().write(("\t\tRefresh of " + activity.dataSourceName + " by user " + activity.userEmail + " on " + activity.accountName + "\r\n").getBytes());
+                    } else {
+                        resp.getOutputStream().write(("\t\t" + activity.name + " by user " + activity.userEmail + " on " + activity.accountName + "\r\n").getBytes());
+                    }
+                }
+            }
+        }
+        resp.getOutputStream().flush();
     }
 }
