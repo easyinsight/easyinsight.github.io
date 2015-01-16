c7bb51fd1f1a4d2624b806ec601e3a0fb383b8c8
James Boe <jboe@easy-insight.com>
2015-01-15 19:41:56 -0700

    pool tweaks

--- java/src/com/easyinsight/userupload/ReportThreadPool.java
+++ java/src/com/easyinsight/userupload/ReportThreadPool.java
@@ -10,14 +10,14 @@ import java.util.concurrent.TimeUnit;
  * Date: 2/11/14
  * Time: 9:21 AM
  */
-public class DataSourceThreadPool {
+public class ReportThreadPool {
     private ThreadPoolExecutor tpe;
     private BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
 
-    private static DataSourceThreadPool instance;
+    private static ReportThreadPool instance;
 
     public static void initialize() {
-        DataSourceThreadPool pool = new DataSourceThreadPool();
+        ReportThreadPool pool = new ReportThreadPool();
         pool.tpe = new ThreadPoolExecutor(5, 5, 5, TimeUnit.MINUTES, pool.queue);
         instance = pool;
     }
@@ -27,17 +27,17 @@ public class DataSourceThreadPool {
     }
 
     public void shutdown() {
-        System.out.println("Shutting down data source thread pool...");
+        System.out.println("Shutting down report thread pool...");
         try {
             tpe.shutdown();
             tpe.awaitTermination(5, TimeUnit.MINUTES);
         } catch (InterruptedException e) {
             // ignore
         }
-        System.out.println("Shut down data source thread pool.");
+        System.out.println("Shut down report thread pool.");
     }
 
-    public static DataSourceThreadPool instance() {
+    public static ReportThreadPool instance() {
         return instance;
     }
 }
