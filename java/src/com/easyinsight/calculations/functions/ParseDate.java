cdac45f2e29cf170de8ee2db2b99cdc7cba3aba9
James Boe <jboe@easy-insight.com>
2015-02-12 14:23:08 -0700

    hrm

--- java/src/com/easyinsight/calculations/functions/ParseDate.java
+++ java/src/com/easyinsight/calculations/functions/ParseDate.java
@@ -4,10 +4,16 @@ import com.easyinsight.calculations.Function;
 import com.easyinsight.core.DateValue;
 import com.easyinsight.core.EmptyValue;
 import com.easyinsight.core.Value;
+import com.easyinsight.logging.LogClass;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -18,6 +24,7 @@ import java.util.Date;
  */
 public class ParseDate extends Function {
     public Value evaluate() {
+        ZoneId zoneId = calculationMetadata.getInsightRequestMetadata().createZoneID();
         try {
             Value start = params.get(0);
             String string = start.toString();
@@ -28,19 +35,22 @@ public class ParseDate extends Function {
                 value = string;
             }
             String format = minusQuotes(1);
-            SimpleDateFormat sdf = new SimpleDateFormat(format);
-            Date date = sdf.parse(value);
-            System.out.println("parsed date = " + date);
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(date);
-            LocalDate localDate = LocalDate.of(cal.get(Calendar.YEAR),
-                    cal.get(Calendar.MONTH) + 1,
-                    cal.get(Calendar.DAY_OF_MONTH));
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format).withZone(zoneId);
+            TemporalAccessor ta = dateTimeFormatter.parse(value);
+            /*SimpleDateFormat sdf = new SimpleDateFormat(format);
+            Date date = sdf.parse(value);*/
+            int dayOfMonth = ta.get(ChronoField.DAY_OF_MONTH);
+            int month = ta.get(ChronoField.MONTH_OF_YEAR);
+            int year = ta.get(ChronoField.YEAR);
+
+            LocalDate localDate = LocalDate.of(year, month, dayOfMonth);
+            Date date = Date.from(localDate.atStartOfDay(zoneId).toInstant());
             DateValue dateValue = new DateValue(date);
             System.out.println("local date = " + localDate);
             dateValue.setLocalDate(localDate);
             return dateValue;
-        } catch (ParseException e) {
+        } catch (Exception e) {
+            LogClass.error(e);
             return new EmptyValue();
         }
     }
