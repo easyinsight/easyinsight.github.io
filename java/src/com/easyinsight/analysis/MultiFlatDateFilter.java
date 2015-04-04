f2f8b116d56fe98846176ea23689cbfa11130376
James Boe <jboe@easy-insight.com>
2015-04-04 11:36:14 -0600

    fix

--- java/src/com/easyinsight/analysis/MultiFlatDateFilter.java
+++ java/src/com/easyinsight/analysis/MultiFlatDateFilter.java
@@ -189,6 +189,7 @@ public class MultiFlatDateFilter extends FilterDefinition {
                 }
                 sb.deleteCharAt(sb.length() - 1);
                 sb.append(")");
+                System.out.println(sb.toString());
                 return sb.toString();
             } else if (level == AnalysisDateDimension.QUARTER_OF_YEAR_LEVEL) {
 
@@ -203,6 +204,7 @@ public class MultiFlatDateFilter extends FilterDefinition {
                 }
                 sb.deleteCharAt(sb.length() - 1);
                 sb.append(")");
+                System.out.println(sb.toString());
                 return sb.toString();
             } else if (level == AnalysisDateDimension.WEEK_LEVEL) {
 
